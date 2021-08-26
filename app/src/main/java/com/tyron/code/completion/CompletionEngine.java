acc04283d38f7c5113bdb7218e5a8c1300a8f9d4
tyron <lucena.tyronscott@gmail.com>
2021-08-26 17:00:12 +0800

    feat: disable completions when indexing

--- app/src/main/java/com/tyron/code/completion/CompletionEngine.java
+++ app/src/main/java/com/tyron/code/completion/CompletionEngine.java
@@ -1,15 +1,18 @@
 package com.tyron.code.completion;
-import com.tyron.code.CompilerProvider;
+
+import android.util.Log;
+
 import com.tyron.code.JavaCompilerService;
-import com.tyron.code.parser.FileManager;
-import java.util.stream.Collectors;
-import java.util.Collections;
 import com.tyron.code.model.CompletionList;
+import com.tyron.code.parser.FileManager;
+
 import java.io.File;
-import com.tyron.code.compiler.CompileBatch;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class CompletionEngine {
-	
+	private static final String TAG = CompletionEngine.class.getSimpleName();
+
 	public static CompletionEngine Instance = null;
 	
 	public static CompletionEngine getInstance() {
@@ -20,6 +23,7 @@ public class CompletionEngine {
 	}
 	
 	private JavaCompilerService mProivider;
+	private boolean mIndexing;
 	
 	private CompletionEngine() {
 		getCompiler();
@@ -31,22 +35,33 @@ public class CompletionEngine {
 		}
 		
 		mProivider = new JavaCompilerService(
-			FileManager.getInstance().getCurrentProject().getJavaFiles().values().stream().collect(Collectors.toSet()),
+				new HashSet<>(FileManager.getInstance().getCurrentProject().getJavaFiles().values()),
 			Collections.emptySet(),
 			Collections.emptySet()
 		);
 		return mProivider;
 	}
-	
+
+	/**
+	 * Disable subsequent completions
+	 */
+	public void setIndexing(boolean val) {
+		mIndexing = val;
+	}
 	public synchronized CompletionList complete(File file, long cursor) {
+		// Do not request for completion if we're indexing
+		if (mIndexing) {
+			return CompletionList.EMPTY;
+		}
+
 		try {
 			return new CompletionProvider(mProivider).complete(file, cursor);
 		} catch (RuntimeException | AssertionError e) {
-			CompileBatch batch = mProivider.cachedCompile;
+			Log.d(TAG, "Completion failed: " + e.getMessage() + " Clearing cache.");
 			mProivider = null;
 			getCompiler();
 			
 		}
-		return new CompletionList();
+		return CompletionList.EMPTY;
 	}
 }
--- app/src/main/java/com/tyron/code/model/CompletionList.java
+++ app/src/main/java/com/tyron/code/model/CompletionList.java
@@ -3,7 +3,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class CompletionList {
-    
+
+    public static final CompletionList EMPTY = new CompletionList();
+
     public boolean isIncomplete = false;
     
     public List<CompletionItem> items = new ArrayList<>();
