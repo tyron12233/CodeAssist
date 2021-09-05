240aae4bf1f6cc772839ee752c852d0219682d2f
tyron <lucena.tyronscott@gmail.com>
2021-09-05 21:06:02 +0800

    feat: added kotlin compiler

---
+++ app/src/main/java/com/tyron/code/compiler/incremental/kotlin/IncrementalKotlinCompiler.java
@@ -0,0 +1,27 @@
+package com.tyron.code.compiler.incremental.kotlin;
+
+public class IncrementalKotlinCompiler {
+    /**
+     * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *
+     *      * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     *      * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *      *
+     *           * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     *      * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *      *
+     *           * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     *      * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *      *
+     *           * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     *      * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *      *
+     *           * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     *      * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *      *
+     *           * GOTEM GOTEM GOTEM GOTEM HA GOTEM
+     *      * GOOOOOOOOOOOOOOOOOOOOOOOOOOOOTEM
+     *      *
+     */
+}
