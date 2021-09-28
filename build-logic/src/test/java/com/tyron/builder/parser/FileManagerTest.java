b62b287496244c7c5057d60c277e25f1fb3fcdf9
tyron12233 <lucena.tyronscott@gmail.com>
2021-09-28 15:40:46 +0800

    feat: add base tests for project

---
+++ build-logic/src/test/java/com/tyron/builder/parser/FileManagerTest.java
@@ -0,0 +1,14 @@
+package com.tyron.builder.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.tyron.builder.BaseProjectTest;
+
+public class FileManagerTest extends BaseProjectTest {
+
+    @Test
+    public void listTest() {
+
+    }
+}
---
+++ build-logic/src/test/resources/TestProject/app/src/main/java/com/tyron/test/MainActivity.java
@@ -0,0 +1,14 @@
+package $packagename;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
