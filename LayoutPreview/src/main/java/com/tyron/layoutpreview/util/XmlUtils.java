41ec9f1f17916eb93968fcf4c1f773c6f366e972
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-03 12:28:08 +0800

    Move common xml logic to XmlUtils

---
+++ LayoutPreview/src/main/java/com/tyron/layoutpreview/util/XmlUtils.java
@@ -0,0 +1,51 @@
+package com.tyron.layoutpreview.util;
+
+import com.tyron.layoutpreview.convert.ConvertException;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+public class XmlUtils {
+
+    public static void advanceToRootNode(XmlPullParser parser) throws IOException, XmlPullParserException, ConvertException {
+        // Look for the root node
+        int type;
+        while ((type = parser.next()) != XmlPullParser.START_TAG &&
+                type != XmlPullParser.END_DOCUMENT) {
+            // Empty
+        }
+
+        if (type != XmlPullParser.START_TAG) {
+            throw new ConvertException(parser.getPositionDescription()
+                    + ": No start tag found!");
+        }
+    }
+
+    public static void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+    public static String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+}
