536d02a6508fbbc522713401230087f951efc0bb
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-03 11:26:33 +0800

    implement string manager in ProteusResources

--- LayoutPreview/src/main/java/com/flipkart/android/proteus/value/Resource.java
+++ LayoutPreview/src/main/java/com/flipkart/android/proteus/value/Resource.java
@@ -23,12 +23,14 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.LruCache;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringDef;
+
+import com.flipkart.android.proteus.ProteusContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * ColorResource
@@ -38,196 +40,212 @@ import androidx.annotation.StringDef;
 
 public class Resource extends Value {
 
-  public static final String RESOURCE_PREFIX_ANIMATION = "@anim/";
-  public static final String RESOURCE_PREFIX_BOOLEAN = "@bool/";
-  public static final String RESOURCE_PREFIX_COLOR = "@color/";
-  public static final String RESOURCE_PREFIX_DIMENSION = "@dimen/";
-  public static final String RESOURCE_PREFIX_DRAWABLE = "@drawable/";
-  public static final String RESOURCE_PREFIX_STRING = "@string/";
+    public static final String RESOURCE_PREFIX_ANIMATION = "@anim/";
+    public static final String RESOURCE_PREFIX_BOOLEAN = "@bool/";
+    public static final String RESOURCE_PREFIX_COLOR = "@color/";
+    public static final String RESOURCE_PREFIX_DIMENSION = "@dimen/";
+    public static final String RESOURCE_PREFIX_DRAWABLE = "@drawable/";
+    public static final String RESOURCE_PREFIX_STRING = "@string/";
 
-  public static final String ANIM = "anim";
-  public static final String BOOLEAN = "bool";
-  public static final String COLOR = "color";
-  public static final String DIMEN = "dimen";
-  public static final String DRAWABLE = "drawable";
-  public static final String STRING = "string";
+    public static final String ANIM = "anim";
+    public static final String BOOLEAN = "bool";
+    public static final String COLOR = "color";
+    public static final String DIMEN = "dimen";
+    public static final String DRAWABLE = "drawable";
+    public static final String STRING = "string";
 
-  public static final Resource NOT_FOUND = new Resource(0);
+    public static final Resource NOT_FOUND = new Resource(0);
 
-  public final int resId;
+    public final int resId;
+    private final String name;
 
-  private Resource(int resId) {
-    this.resId = resId;
-  }
-
-  public static boolean isAnimation(String string) {
-    return string.startsWith(RESOURCE_PREFIX_ANIMATION);
-  }
-
-  public static boolean isBoolean(String string) {
-    return string.startsWith(RESOURCE_PREFIX_BOOLEAN);
-  }
-
-  public static boolean isColor(String string) {
-    return string.startsWith(RESOURCE_PREFIX_COLOR);
-  }
-
-  public static boolean isDimension(String string) {
-    return string.startsWith(RESOURCE_PREFIX_DIMENSION);
-  }
-
-  public static boolean isDrawable(String string) {
-    return string.startsWith(RESOURCE_PREFIX_DRAWABLE);
-  }
-
-  public static boolean isString(String string) {
-    return string.startsWith(RESOURCE_PREFIX_STRING);
-  }
-
-  public static boolean isResource(String string) {
-    return isAnimation(string) || isBoolean(string) || isColor(string) || isDimension(string) || isDrawable(string) || isString(string);
-  }
-
-  @Nullable
-  public static Boolean getBoolean(int resId, Context context) {
-    try {
-      return context.getResources().getBoolean(resId);
-    } catch (Resources.NotFoundException e) {
-      return null;
+    @Deprecated
+    private Resource(int resId, String name) {
+        this.resId = resId;
+        this.name = name;
     }
-  }
 
-  @Nullable
-  public static Integer getColor(int resId, Context context) {
-    try {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        return context.getResources().getColor(resId, context.getTheme());
-      } else {
-        //noinspection deprecation
-        return context.getResources().getColor(resId);
-      }
-    } catch (Resources.NotFoundException e) {
-      return null;
+    private Resource(int id) {
+        this(id, null);
     }
-  }
 
-  @Nullable
-  public static ColorStateList getColorStateList(int resId, Context context) {
-    try {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        return context.getColorStateList(resId);
-      } else {
-        //noinspection deprecation
-        return context.getResources().getColorStateList(resId);
-      }
-
-    } catch (Resources.NotFoundException nfe) {
-      return null;
+    public static boolean isAnimation(String string) {
+        return string.startsWith(RESOURCE_PREFIX_ANIMATION);
     }
-  }
 
-  @Nullable
-  public static Drawable getDrawable(int resId, Context context) {
-    try {
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-        return context.getResources().getDrawable(resId, context.getTheme());
-      } else {
-        //noinspection deprecation
-        return context.getResources().getDrawable(resId);
-      }
-    } catch (Resources.NotFoundException e) {
-      return null;
+    public static boolean isBoolean(String string) {
+        return string.startsWith(RESOURCE_PREFIX_BOOLEAN);
     }
-  }
 
-  @Nullable
-  public static Float getDimension(int resId, Context context) {
-    try {
-      return context.getResources().getDimension(resId);
-    } catch (Resources.NotFoundException e) {
-      return null;
+    public static boolean isColor(String string) {
+        return string.startsWith(RESOURCE_PREFIX_COLOR);
     }
-  }
 
-  @Nullable
-  public static String getString(int resId, Context context) {
-    try {
-      return context.getString(resId);
-    } catch (Resources.NotFoundException e) {
-      return null;
+    public static boolean isDimension(String string) {
+        return string.startsWith(RESOURCE_PREFIX_DIMENSION);
     }
-  }
 
-  @Nullable
-  public static Resource valueOf(String value, @Nullable @ResourceType String type, Context context) {
-    if (null == value) {
-      return null;
+    public static boolean isDrawable(String string) {
+        return string.startsWith(RESOURCE_PREFIX_DRAWABLE);
     }
-    Resource resource = ResourceCache.cache.get(value);
-    if (null == resource) {
-      int resId = context.getResources().getIdentifier(value, type, context.getPackageName());
-      resource = resId == 0 ? NOT_FOUND : new Resource(resId);
-      ResourceCache.cache.put(value, resource);
+
+    public static boolean isString(String string) {
+        return string.startsWith(RESOURCE_PREFIX_STRING);
     }
-    return NOT_FOUND == resource ? null : resource;
-  }
 
-  @NonNull
-  public static Resource valueOf(int resId) {
-    return new Resource(resId);
-  }
+    public static boolean isResource(String string) {
+        return isAnimation(string) || isBoolean(string) || isColor(string) || isDimension(string) || isDrawable(string) || isString(string);
+    }
 
-  @Nullable
-  public Boolean getBoolean(Context context) {
-    return getBoolean(resId, context);
-  }
+    @Nullable
+    public static Boolean getBoolean(int resId, Context context) {
+        try {
+            return context.getResources().getBoolean(resId);
+        } catch (Resources.NotFoundException e) {
+            return null;
+        }
+    }
 
-  @Nullable
-  public Integer getColor(Context context) {
-    return getColor(resId, context);
-  }
+    @Nullable
+    public static Integer getColor(int resId, Context context) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return context.getResources().getColor(resId, context.getTheme());
+            } else {
+                //noinspection deprecation
+                return context.getResources().getColor(resId);
+            }
+        } catch (Resources.NotFoundException e) {
+            return null;
+        }
+    }
 
-  @Nullable
-  public ColorStateList getColorStateList(Context context) {
-    return getColorStateList(resId, context);
-  }
+    @Nullable
+    public static ColorStateList getColorStateList(int resId, Context context) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return context.getColorStateList(resId);
+            } else {
+                //noinspection deprecation
+                return context.getResources().getColorStateList(resId);
+            }
 
-  @Nullable
-  public Drawable getDrawable(Context context) {
-    return getDrawable(resId, context);
-  }
+        } catch (Resources.NotFoundException nfe) {
+            return null;
+        }
+    }
 
-  @Nullable
-  public Float getDimension(Context context) {
-    return getDimension(resId, context);
-  }
+    @Nullable
+    public static Drawable getDrawable(int resId, Context context) {
+        try {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                return context.getResources().getDrawable(resId, context.getTheme());
+            } else {
+                //noinspection deprecation
+                return context.getResources().getDrawable(resId);
+            }
+        } catch (Resources.NotFoundException e) {
+            return null;
+        }
+    }
 
-  @Nullable
-  public Integer getInteger(Context context) {
-    return getInteger(resId, context);
-  }
+    @Nullable
+    public static Float getDimension(int resId, Context context) {
+        try {
+            return context.getResources().getDimension(resId);
+        } catch (Resources.NotFoundException e) {
+            return null;
+        }
+    }
 
-  @Nullable
-  public String getString(Context context) {
-    return getString(resId, context);
-  }
+    @Nullable
+    public static String getString(int resId, Context context) {
+        try {
+            return context.getString(resId);
+        } catch (Resources.NotFoundException e) {
+            return null;
+        }
+    }
 
-  @Nullable
-  public Integer getInteger(int resId, Context context) {
-    return context.getResources().getInteger(resId);
-  }
 
-  @Override
-  public Value copy() {
-    return this;
-  }
+    @Nullable
+    public static Resource valueOf(String value, @Nullable @ResourceType String type, Context context) {
+        if (null == value) {
+            return null;
+        }
+        Resource resource = ResourceCache.cache.get(value);
+        if (null == resource) {
+            int resId = context.getResources().getIdentifier(value, type, context.getPackageName());
+            resource = resId == 0 ? NOT_FOUND : new Resource(resId, value);
+            ResourceCache.cache.put(value, resource);
+        }
+        return NOT_FOUND == resource ? null : resource;
+    }
 
-  @StringDef({ANIM, BOOLEAN, COLOR, DRAWABLE, DIMEN, STRING})
-  @Retention(RetentionPolicy.SOURCE)
-  public @interface ResourceType {
-  }
+    @NonNull
+    public static Resource valueOf(int resId) {
+        return new Resource(resId);
+    }
 
-  private static class ResourceCache {
-    static final LruCache<String, Resource> cache = new LruCache<>(64);
-  }
+    @Nullable
+    public Boolean getBoolean(Context context) {
+        return getBoolean(resId, context);
+    }
+
+    @Nullable
+    public Integer getColor(Context context) {
+        return getColor(resId, context);
+    }
+
+    @Nullable
+    public ColorStateList getColorStateList(Context context) {
+        return getColorStateList(resId, context);
+    }
+
+    @Nullable
+    public Drawable getDrawable(Context context) {
+        return getDrawable(resId, context);
+    }
+
+    @Nullable
+    public Float getDimension(Context context) {
+        return getDimension(resId, context);
+    }
+
+    @Nullable
+    public Integer getInteger(Context context) {
+        return getInteger(resId, context);
+    }
+
+    @Nullable
+    public String getString(ProteusContext context) {
+        String value;
+        if (name != null) {
+            Value string = context.getProteusResources()
+                    .getString(name);
+            value = null != string ? string.getAsString() : null;
+        } else {
+            value = getString(resId, context);
+        }
+        return value;
+    }
+
+    @Nullable
+    public Integer getInteger(int resId, Context context) {
+        return context.getResources().getInteger(resId);
+    }
+
+    @Override
+    public Value copy() {
+        return this;
+    }
+
+    @StringDef({ANIM, BOOLEAN, COLOR, DRAWABLE, DIMEN, STRING})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ResourceType {
+    }
+
+    private static class ResourceCache {
+        static final LruCache<String, Resource> cache = new LruCache<>(64);
+    }
 }
