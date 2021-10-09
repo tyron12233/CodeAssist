dcab9fde967e424ff4a8535ad124c7adf3047496
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-09 18:04:50 +0800

    Add clean task for removing unused files

---
+++ build-logic/src/main/java/com/tyron/builder/compiler/CleanTask.java
@@ -0,0 +1,90 @@
+package com.tyron.builder.compiler;
+
+import com.tyron.builder.exception.CompilationFailedException;
+import com.tyron.builder.log.ILogger;
+import com.tyron.builder.model.Project;
+import com.tyron.builder.parser.FileManager;
+import com.tyron.common.util.Cache;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class CleanTask extends Task {
+
+    private static final String TAG = CleanTask.class.getSimpleName();
+
+    private Project mProject;
+    private ILogger mLogger;
+    private Cache<String, List<File>> classCache;
+
+    @Override
+    public String getName() {
+        return TAG;
+    }
+
+    @Override
+    public void prepare(Project project, ILogger logger, BuildType type) throws IOException {
+        mProject = project;
+        mLogger = logger;
+        classCache = FileManager.getInstance().getClassCache();
+    }
+
+    @Override
+    public void run() throws IOException, CompilationFailedException {
+        cleanClasses();
+        cleanDexFiles();
+    }
+
+    private void cleanClasses() {
+
+        File output = new File(mProject.getBuildDirectory(), "bin/classes/");
+        List<File> classFiles = FileManager.findFilesWithExtension(
+                output,
+                ".class");
+        for (File file : classFiles) {
+            String path = file.getAbsolutePath()
+                    .replace(output.getAbsolutePath(), "")
+                    .replace("/", ".")
+                    .substring(1)
+                    .replace(".class", "");
+
+            String packageName = path;
+            if (path.contains("$")) {
+                path = path.substring(0, path.lastIndexOf("$"));
+            }
+
+            if (FileManager.getInstance().list(path).isEmpty() && !FileManager.getInstance().containsClass(packageName)) {
+                if (file.delete()) {
+                    mLogger.debug("Deleted class file " + file.getName());
+                }
+            }
+        }
+    }
+
+    private void cleanDexFiles() {
+        File output = new File(mProject.getBuildDirectory(), "intermediate/classes");
+        List<File> classFiles = FileManager.findFilesWithExtension(
+                output,
+                ".dex");
+
+        for (File file : classFiles) {
+            String path = file.getAbsolutePath()
+                    .replace(output.getAbsolutePath(), "")
+                    .replace("/", ".")
+                    .substring(1)
+                    .replace(".dex", "");
+            String packageName = path;
+            if (path.contains("$")) {
+                path = path.substring(0, path.lastIndexOf("$"));
+            }
+
+            if (FileManager.getInstance().list(path).isEmpty() && !FileManager.getInstance().containsClass(packageName)) {
+                if (file.delete()) {
+                    mLogger.debug("Deleted dex file " + file.getName());
+                }
+            }
+        }
+    }
+}
