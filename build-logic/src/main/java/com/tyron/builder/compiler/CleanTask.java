adeede7261ae70f55ea3266b04a6d6670fac3970
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-10 10:54:13 +0800

    Clean all intermediate data on a release build

--- build-logic/src/main/java/com/tyron/builder/compiler/CleanTask.java
+++ build-logic/src/main/java/com/tyron/builder/compiler/CleanTask.java
@@ -6,6 +6,8 @@ import com.tyron.builder.model.Project;
 import com.tyron.builder.parser.FileManager;
 import com.tyron.common.util.Cache;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -17,7 +19,7 @@ public class CleanTask extends Task {
 
     private Project mProject;
     private ILogger mLogger;
-    private Cache<String, List<File>> classCache;
+    private BuildType mBuildType;
 
     @Override
     public String getName() {
@@ -28,15 +30,36 @@ public class CleanTask extends Task {
     public void prepare(Project project, ILogger logger, BuildType type) throws IOException {
         mProject = project;
         mLogger = logger;
-        classCache = FileManager.getInstance().getClassCache();
+        mBuildType = type;
     }
 
     @Override
     public void run() throws IOException, CompilationFailedException {
-        cleanClasses();
-        cleanDexFiles();
+        if (mBuildType == BuildType.RELEASE) {
+            cleanRelease();
+        } else if (mBuildType == BuildType.DEBUG) {
+            cleanClasses();
+            cleanDexFiles();
+        }
     }
 
+    private void cleanRelease() throws IOException {
+        File binDirectory = new File(mProject.getBuildDirectory(), "bin");
+        FileUtils.delete(binDirectory);
+
+        File genDirectory = new File(mProject.getBuildDirectory(), "gen");
+        FileUtils.delete(genDirectory);
+
+        File intermediateDirectory = new File(mProject.getBuildDirectory(), "intermediate");
+        FileUtils.delete(intermediateDirectory);
+
+        FileManager.getInstance().getClassCache()
+                .clear();
+        FileManager.getInstance().getDexCache()
+                .clear();
+        FileManager.getInstance().getSymbolCache()
+                .clear();
+    }
     private void cleanClasses() {
 
         File output = new File(mProject.getBuildDirectory(), "bin/classes/");
