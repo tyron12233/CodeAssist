e723d93bc6a49191c2a7c4351e03544f9b6392b8
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-11 23:52:47 +0800

    Disable kotlin completions if disabled in preferences

--- app/src/main/java/com/tyron/code/ui/editor/language/kotlin/KotlinAutoCompleteProvider.java
+++ app/src/main/java/com/tyron/code/ui/editor/language/kotlin/KotlinAutoCompleteProvider.java
@@ -1,10 +1,15 @@
 package com.tyron.code.ui.editor.language.kotlin;
 
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
 import com.tyron.builder.parser.FileManager;
 import com.tyron.completion.model.CompletionList;
 import com.tyron.kotlin_completion.CompletionEngine;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import io.github.rosemoe.sora.interfaces.AutoCompleteProvider;
@@ -16,15 +21,22 @@ public class KotlinAutoCompleteProvider implements AutoCompleteProvider {
 
     private CompletionEngine engine;
     private final CodeEditor mEditor;
+    private final SharedPreferences mPreferences;
+
 
     public KotlinAutoCompleteProvider(CodeEditor editor) {
         mEditor = editor;
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(editor.getContext());
     }
 
     @Override
     public List<CompletionItem> getAutoCompleteItems(String prefix, TextAnalyzeResult analyzeResult, int line, int column) {
         if (com.tyron.completion.provider.CompletionEngine.isIndexing()) {
             return null;
+        }
+
+        if (!mPreferences.getBoolean("code_editor_completion", true)) {
+            return Collections.emptyList();
         }
 
         if (engine == null) {
