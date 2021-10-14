a5546584c5bacde0852f0f95b51339dc285b3b62
tyron <lucena.tyronscott@gmail.com>
2021-10-14 15:17:55 +0800

    Set Kind of logs based on type

--- app/src/main/java/com/tyron/code/ui/editor/log/AppLogFragment.java
+++ app/src/main/java/com/tyron/code/ui/editor/log/AppLogFragment.java
@@ -102,12 +102,15 @@ public class AppLogFragment extends Fragment {
                         DiagnosticWrapper wrapped = ILogger.wrap(message);
                         switch (type) {
                             case "DEBUG":
+                                wrapped.setKind(Diagnostic.Kind.NOTE);
                                 model.d(LogViewModel.APP_LOG, wrapped);
                                 break;
                             case "ERROR":
+                                wrapped.setKind(Diagnostic.Kind.ERROR);
                                 model.e(LogViewModel.APP_LOG, wrapped);
                                 break;
                             case "WARNING":
+                                wrapped.setKind(Diagnostic.Kind.WARNING);
                                 model.w(LogViewModel.APP_LOG, wrapped);
                                 break;
                             case "INFO":
