1e2adc963aa868e55021cb7b5b36eb0bb4008b69
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-17 17:32:38 +0800

    Use ProjectManagerFragment as the default screen

--- app/src/main/java/com/tyron/code/MainActivity.java
+++ app/src/main/java/com/tyron/code/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.tyron.code.ui.main.MainFragment;
+import com.tyron.code.ui.project.ProjectManagerFragment;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
         
        //x requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
 
-        if (getSupportFragmentManager().findFragmentByTag("main_fragment") == null) {
+        if (getSupportFragmentManager().findFragmentByTag(ProjectManagerFragment.TAG) == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new MainFragment(), "main_fragment")
+                    .replace(R.id.fragment_container, new ProjectManagerFragment(), ProjectManagerFragment.TAG)
                     .commit();
         }
     }
