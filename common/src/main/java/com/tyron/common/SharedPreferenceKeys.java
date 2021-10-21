119d4ef0793a576e5f2900a0d865cc44ede53ed7
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-21 18:41:29 +0800

    feat: add options to toggle keyboard suggestions

--- common/src/main/java/com/tyron/common/SharedPreferenceKeys.java
+++ common/src/main/java/com/tyron/common/SharedPreferenceKeys.java
@@ -3,4 +3,6 @@ package com.tyron.common;
 public class SharedPreferenceKeys {
 
     public static final String PROJECT_SAVE_PATH = "save_path";
+    public static final String KEYBOARD_ENABLE_SUGGESTIONS = "keyboard_enable_suggestions";
+    public static final String FONT_SIZE = "font_size";
 }
