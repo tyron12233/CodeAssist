a13d14855b382ea136e43879416339dfc2868f20
tyron12233 <lucena.tyronscott@gmail.com>
2021-10-24 17:59:44 +0800

    feat: add methods for hashing files

--- app/src/main/java/com/tyron/code/util/AndroidUtilities.java
+++ app/src/main/java/com/tyron/code/util/AndroidUtilities.java
@@ -13,6 +13,15 @@ import android.view.inputmethod.InputMethodManager;
 
 import com.tyron.code.ApplicationLoader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 @SuppressWarnings("unused")
 public class AndroidUtilities {
     
@@ -43,6 +52,50 @@ public class AndroidUtilities {
 			imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
 		} catch (Exception e) {
 			Log.d("AndroidUtilities", "Failed to close keyboard " + e.getMessage());
+		}
+	}
+
+	public static String calculateMD5(File updateFile) {
+		InputStream is;
+		try {
+			is = new FileInputStream(updateFile);
+		} catch (FileNotFoundException e) {
+			Log.e("calculateMD5", "Exception while getting FileInputStream", e);
+			return null;
+		}
+
+		return calculateMD5(is);
+	}
+
+	public static String calculateMD5(InputStream is) {
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			Log.e("calculateMD5", "Exception while getting Digest", e);
+			return null;
+		}
+
+		byte[] buffer = new byte[8192];
+		int read;
+		try {
+			while ((read = is.read(buffer)) > 0) {
+				digest.update(buffer, 0, read);
+			}
+			byte[] md5sum = digest.digest();
+			BigInteger bigInt = new BigInteger(1, md5sum);
+			String output = bigInt.toString(16);
+			// Fill to 32 chars
+			output = String.format("%32s", output).replace(' ', '0');
+			return output;
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to process file for MD5", e);
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				Log.e("calculateMD5", "Exception on closing MD5 input stream", e);
+			}
 		}
 	}
 
