8c04beefc2dbc999398947e7bc154754d085d04f
tyron12233 <lucena.tyronscott@gmail.com>
2021-11-29 23:55:24 +0800

    test, fix: use resources as test data

--- build-logic/src/test/java/com/tyron/builder/project/ProjectTest.java
+++ build-logic/src/test/java/com/tyron/builder/project/ProjectTest.java
@@ -1,21 +1,43 @@
 package com.tyron.builder.project;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import com.tyron.builder.compiler.manifest.xml.ManifestData;
+import com.tyron.builder.project.api.Project;
+import com.tyron.builder.project.impl.ProjectImpl;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
 public class ProjectTest {
 
-    private static final File ROOT = new File("/home/tyron/AndroidStudioProjects/CodeAssist");
+    private Project mProject;
+
+    @Before
+    public void setupProject() {
+        String rootPath = getClass().getClassLoader()
+                .getResource("TestProject")
+                .getFile();
+        File rootFile = new File(rootPath);
+        mProject = new ProjectImpl(rootFile);
+    }
 
     @Test
     public void init() throws IOException {
-        Project project = new ProjectImpl(ROOT);
-        project.open();
+        mProject.open();
+
+        ManifestData manifestData = mProject.getUserData(CommonProjectKeys.MANIFEST_DATA_KEY);
+        assertThat(manifestData).isNotNull();
+
+        List<File> javaFiles = mProject.getUserData(CommonProjectKeys.JAVA_FILES_KEY);
+        assertThat(javaFiles).isNotEmpty();
+        assertThat(javaFiles).hasSize(1);
     }
 }
