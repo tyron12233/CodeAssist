a559042f1a91bf08e2a3ba155e291273f07436ae
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-01 01:09:55 +0800

    test: add AndroidAppBuilderTest

--- build-logic/src/test/java/com/tyron/builder/compiler/AndroidAppBuilderTest.java
+++ build-logic/src/test/java/com/tyron/builder/compiler/AndroidAppBuilderTest.java
@@ -1,8 +1,13 @@
 package com.tyron.builder.compiler;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import com.tyron.builder.compiler.incremental.resource.IncrementalAapt2Task;
 import com.tyron.builder.log.ILogger;
 import com.tyron.builder.project.impl.MockAndroidProject;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,24 +17,36 @@ import java.io.IOException;
 public class AndroidAppBuilderTest {
 
     private MockAndroidProject mProject;
+    private File mResourcesDir;
 
     @Before
     public void setup() throws Exception {
-        File resourcesDir = ProjectUtil.getResourcesDirectory();
-        File projectDir = new File(resourcesDir, "TestProject");
+        mResourcesDir = ProjectUtil.getResourcesDirectory();
+        File projectDir = new File(mResourcesDir, "TestProject");
         mProject = new MockAndroidProject(projectDir);
-        mProject.setLambdaStubsJarFile(new File(resourcesDir, "bootstraps/core-lambda-stubs.jar"));
-        mProject.setBootstrapFile(new File(resourcesDir, "bootstraps/rt.jar"));
+        mProject.setLambdaStubsJarFile(new File(mResourcesDir, "bootstraps/core-lambda-stubs.jar"));
+        mProject.setBootstrapFile(new File(mResourcesDir, "bootstraps/rt.jar"));
     }
 
     @Test
     public void testBuild() throws Exception {
+        File aapt2 = new File(mResourcesDir, "aapt2/libaapt2.so");
+        assert aapt2.setExecutable(true);
+        IncrementalAapt2Task.setAapt2Binary(aapt2);
+
+        ApkSigner.setTestCertFile(new File(mResourcesDir, "apksigner/testkey.x509.pem"));
+        ApkSigner.setTestKeyFile(new File(mResourcesDir, "apksigner/testkey.pk8"));
+
         mProject.addJavaFile(new File(mProject.getJavaDirectory(),
                 "com/tyron/test/MainActivity.java"));
-
         mProject.open();
 
         AndroidAppBuilder builder = new AndroidAppBuilder(mProject, ILogger.STD_OUT);
         builder.build(BuildType.RELEASE);
+
+        File signedApk = new File(mProject.getBuildDirectory(), "bin/signed.apk");
+        assertThat(signedApk.exists()).isTrue();
+
+        FileUtils.deleteDirectory(mProject.getBuildDirectory());
     }
 }
