a8e5e25c4695a92250c980f9d311e666726ddf03
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-01 00:06:52 +0800

    feat: update builder interface

--- build-logic/src/main/java/com/tyron/builder/compiler/Builder.java
+++ build-logic/src/main/java/com/tyron/builder/compiler/Builder.java
@@ -4,11 +4,16 @@ import com.tyron.builder.exception.CompilationFailedException;
 import com.tyron.builder.log.ILogger;
 import com.tyron.builder.project.api.Project;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface Builder<T extends Project> {
 
     T getProject();
 
-    void build(BuildType type) throws CompilationFailedException;
+    void build(BuildType type) throws CompilationFailedException, IOException;
 
     ILogger getLogger();
+
+    List<Task<T>> getTasks();
 }
