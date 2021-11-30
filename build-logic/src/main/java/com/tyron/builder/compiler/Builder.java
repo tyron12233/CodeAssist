a6a52ac1884edd678201183166c100316edcfcab
tyron12233 <lucena.tyronscott@gmail.com>
2021-11-30 23:26:36 +0800

    feat: add builder interface

--- build-logic/src/main/java/com/tyron/builder/compiler/Builder.java
+++ build-logic/src/main/java/com/tyron/builder/compiler/Builder.java
@@ -1,4 +1,14 @@
 package com.tyron.builder.compiler;
 
-public class Builder {
+import com.tyron.builder.exception.CompilationFailedException;
+import com.tyron.builder.log.ILogger;
+import com.tyron.builder.project.api.Project;
+
+public interface Builder<T extends Project> {
+
+    T getProject();
+
+    void build(BuildType type) throws CompilationFailedException;
+
+    ILogger getLogger();
 }
