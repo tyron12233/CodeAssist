84d3fff4886ff0d4e520572f8d59ec2fbbc53604
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-01 23:10:39 +0800

    test: add more completion tests

---
+++ completion/src/test/resources/EmptyProject/completion/InherittedMembers.java
@@ -0,0 +1,11 @@
+package com.tyron.test;
+
+/**
+ * Tests that members are visible to its subclasses
+ */
+public class InherittedMembers {
+
+    public class Parent {
+        public static
+    }
+}
