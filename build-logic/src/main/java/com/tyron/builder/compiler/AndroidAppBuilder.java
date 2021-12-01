f83696de08c960b675a2b7c4dd361beda27c3499
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-01 18:29:48 +0800

    feat: add logger and r8 tasks

--- build-logic/src/main/java/com/tyron/builder/compiler/AndroidAppBuilder.java
+++ build-logic/src/main/java/com/tyron/builder/compiler/AndroidAppBuilder.java
@@ -2,14 +2,18 @@ package com.tyron.builder.compiler;
 
 import com.tyron.builder.compiler.apk.PackageTask;
 import com.tyron.builder.compiler.apk.SignTask;
+import com.tyron.builder.compiler.dex.D8Task;
+import com.tyron.builder.compiler.dex.R8Task;
 import com.tyron.builder.compiler.firebase.GenerateFirebaseConfigTask;
 import com.tyron.builder.compiler.incremental.dex.IncrementalD8Task;
 import com.tyron.builder.compiler.incremental.java.IncrementalJavaTask;
 import com.tyron.builder.compiler.incremental.kotlin.IncrementalKotlinCompiler;
 import com.tyron.builder.compiler.incremental.resource.IncrementalAapt2Task;
+import com.tyron.builder.compiler.log.InjectLoggerTask;
 import com.tyron.builder.compiler.manifest.ManifestMergeTask;
 import com.tyron.builder.compiler.symbol.MergeSymbolsTask;
 import com.tyron.builder.log.ILogger;
+import com.tyron.builder.model.ProjectSettings;
 import com.tyron.builder.project.api.AndroidProject;
 
 import java.util.ArrayList;
@@ -27,10 +31,19 @@ public class AndroidAppBuilder extends BuilderImpl<AndroidProject> {
         tasks.add(new CleanTask(getProject(), getLogger()));
         tasks.add(new ManifestMergeTask(getProject(), getLogger()));
         tasks.add(new GenerateFirebaseConfigTask(getProject(), getLogger()));
+        if (type == BuildType.DEBUG) {
+            tasks.add(new InjectLoggerTask(getProject(), getLogger()));
+        }
         tasks.add(new IncrementalAapt2Task(getProject(), getLogger(), false));
         tasks.add(new MergeSymbolsTask(getProject(), getLogger()));
         tasks.add(new IncrementalKotlinCompiler(getProject(), getLogger()));
         tasks.add(new IncrementalJavaTask(getProject(), getLogger()));
+        if (getProject().getSettings().getBoolean(ProjectSettings.USE_R8, false) &&
+                type == BuildType.RELEASE) {
+            tasks.add(new R8Task(getProject(), getLogger()));
+        } else {
+            tasks.add(new D8Task(getProject(), getLogger()));
+        }
         tasks.add(new IncrementalD8Task(getProject(), getLogger()));
         tasks.add(new PackageTask(getProject(), getLogger()));
         tasks.add(new SignTask(getProject(), getLogger()));
