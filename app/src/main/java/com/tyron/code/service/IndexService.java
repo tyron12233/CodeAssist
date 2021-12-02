62bb673259964c0c5ad35dcdeb9e14322cd1e1e3
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-02 16:16:40 +0800

    fix: invoke callbacks on main thread

--- app/src/main/java/com/tyron/code/service/IndexService.java
+++ app/src/main/java/com/tyron/code/service/IndexService.java
@@ -4,7 +4,9 @@ import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 
 import androidx.core.app.NotificationChannelCompat;
 import androidx.core.app.NotificationCompat;
@@ -19,6 +21,7 @@ public class IndexService extends Service {
 
     private static final int NOTIFICATION_ID = 23;
 
+    private final Handler mMainHandler = new Handler(Looper.getMainLooper());
     private final IndexBinder mBinder = new IndexBinder();
 
     public IndexService() {
@@ -59,12 +62,12 @@ public class IndexService extends Service {
                         .setContentText(message)
                         .build();
                 updateNotification(notification);
-                listener.onTaskStarted(message);
+                mMainHandler.post(() -> listener.onTaskStarted(message));
             }
 
             @Override
             public void onComplete(boolean success, String message) {
-                listener.onComplete(success, message);
+                mMainHandler.post(() -> listener.onComplete(success, message));
                 stopForeground(true);
                 stopSelf();
             }
