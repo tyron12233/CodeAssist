05270d3611202bf3d18cef08d0dde2d54519115d
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-08 03:06:32 +0800

    test: update test data to include known android widgets

---
+++ layout-preview/src/test/java/com/tyron/layoutpreview/convert/TestInflateXml.java
@@ -0,0 +1,92 @@
+package com.tyron.layoutpreview.convert;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.flipkart.android.proteus.Proteus;
+import com.flipkart.android.proteus.ProteusBuilder;
+import com.flipkart.android.proteus.ProteusContext;
+import com.flipkart.android.proteus.ProteusLayoutInflater;
+import com.flipkart.android.proteus.ProteusView;
+import com.flipkart.android.proteus.SimpleLayoutInflater;
+import com.flipkart.android.proteus.ViewTypeParser;
+import com.flipkart.android.proteus.processor.AttributeProcessor;
+import com.flipkart.android.proteus.value.Layout;
+import com.flipkart.android.proteus.value.ObjectValue;
+import com.flipkart.android.proteus.value.Value;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import com.tyron.layoutpreview.convert.adapter.ProteusTypeAdapterFactory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.io.StringReader;
+import java.util.Map;
+
+@RunWith(RobolectricTestRunner.class)
+public class TestInflateXml {
+
+    private static final String TEST_LAYOUT = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + "<androidx.constraintlayout.widget.ConstraintLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" + "    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n" + "    xmlns:tools=\"http://schemas.android.com/tools\"\n" + "    android:layout_width=\"match_parent\"\n" + "    android:layout_height=\"match_parent\">\n" + "\n" + "    <com.google.android.material.textfield.TextInputLayout\n" + "        android:id=\"@+id/til_class_name\"\n" + "        style=\"@style/Widget.MaterialComponents.TextInputLayout.FilledBox.Dense\"\n" + "        android:layout_width=\"0dp\"\n" + "        android:layout_height=\"wrap_content\"\n" + "        android:layout_marginStart=\"16dp\"\n" + "        android:layout_marginTop=\"16dp\"\n" + "        android:layout_marginEnd=\"16dp\"\n" + "        app:layout_constraintEnd_toEndOf=\"parent\"\n" + "        app:layout_constraintStart_toStartOf=\"parent\"\n" + "        app:layout_constraintTop_toTopOf=\"parent\">\n" + "\n" + "        <com.google.android.material.textfield.TextInputEditText\n" + "            android:id=\"@+id/et_class_name\"\n" + "            android:layout_width=\"match_parent\"\n" + "            android:layout_height=\"wrap_content\"\n" + "            android:hint=\"@string/create_class_dialog_class_name\" />\n" + "    </com.google.android.material.textfield.TextInputLayout>\n" + "\n" + "    <com.google.android.material.textfield.TextInputLayout\n" + "        style=\"@style/Widget.MaterialComponents.TextInputLayout.FilledBox.Dense.ExposedDropdownMenu\"\n" + "        android:layout_width=\"0dp\"\n" + "        android:layout_height=\"wrap_content\"\n" + "        android:layout_marginStart=\"16dp\"\n" + "        android:layout_marginTop=\"8dp\"\n" + "        android:layout_marginEnd=\"16dp\"\n" + "        android:layout_marginBottom=\"16dp\"\n" + "        app:layout_constraintBottom_toBottomOf=\"parent\"\n" + "        app:layout_constraintEnd_toEndOf=\"parent\"\n" + "        app:layout_constraintStart_toStartOf=\"parent\"\n" + "        app:layout_constraintTop_toBottomOf=\"@+id/til_class_name\"\n" + "        app:layout_constraintVertical_bias=\"0.0\">\n" + "\n" + "        <com.google.android.material.textfield.MaterialAutoCompleteTextView\n" + "            android:id=\"@+id/et_class_type\"\n" + "            android:layout_width=\"match_parent\"\n" + "            android:layout_height=\"match_parent\"\n" + "            android:hint=\"@string/create_class_dialog_class_type\"\n" + "            android:inputType=\"none\"/>\n" + "    </com.google.android.material.textfield.TextInputLayout>\n" + "</androidx.constraintlayout.widget.ConstraintLayout>";
+
+    private final ProteusLayoutInflater.Callback mCallback = new ProteusLayoutInflater.Callback() {
+        @NonNull
+        @Override
+        public ProteusView onUnknownViewType(ProteusContext context, ViewGroup parent, String type, Layout layout, ObjectValue data, int index) {
+            ProteusView proteusView = new ProteusView() {
+                @Override
+                public Manager getViewManager() {
+                    return null;
+                }
+
+                @Override
+                public void setViewManager(@NonNull Manager manager) {
+
+                }
+
+                @NonNull
+                @Override
+                public View getAsView() {
+                    return null;
+                }
+            };
+            return proteusView;
+        }
+
+        @Override
+        public void onEvent(String event, Value value, ProteusView view) {
+
+        }
+    };
+
+    @Test
+    public void test() throws Exception {
+        JsonObject object = new XmlToJsonConverter().convert(TEST_LAYOUT);
+
+        ProteusBuilder builder = new ProteusBuilder();
+        Proteus proteus = builder.build();
+        ProteusContext context = proteus.createContextBuilder(ApplicationProvider.getApplicationContext())
+                .setCallback(mCallback)
+                .build();
+        ProteusTypeAdapterFactory.PROTEUS_INSTANCE_HOLDER.setProteus(proteus);
+
+        Value read = new ProteusTypeAdapterFactory(context).VALUE_TYPE_ADAPTER.read(new JsonReader(
+                new StringReader(object.toString())
+        ));
+
+        Layout layout = read.getAsLayout();
+        ProteusView inflate = context.getInflater().inflate(layout, new ObjectValue());
+
+        Map<String, ViewTypeParser.AttributeSet.Attribute> attributes = context.getParser("android.widget.FrameLayout")
+                .getAttributeSet()
+                .getAttributes();
+        ViewTypeParser.AttributeSet.Attribute attribute = attributes.get("layout_gravity");
+        AttributeProcessor processor = attribute.processor;
+        String name = processor.getClass().getSuperclass().getName();
+        assert inflate != null;
+    }
+}
