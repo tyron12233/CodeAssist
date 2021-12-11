7849466f65005c1d43c6ab67525203cca854d5e6
tyron12233 <74818961+tyron12233@users.noreply.github.com>
2021-12-11 20:15:41 +0800

    feat: use fuzzy search for completion items

--- common/src/main/java/com/tyron/common/util/StringSearch.java
+++ common/src/main/java/com/tyron/common/util/StringSearch.java
@@ -207,11 +207,12 @@ public class StringSearch {
 		if (partialName.length() == 1 && partialName.equals(".")) {
 			return true;
 		}
-        if (candidate.length() < partialName.length()) return false;
+      /*  if (candidate.length() < partialName.length()) return false;
         for (int i = 0; i < partialName.length(); i++) {
             if (candidate.charAt(i) != partialName.charAt(i)) return false;
         }
-        return true;
+        return true;*/
+        return similarity(candidate, partialName) > 0.5;
     }
     
     public static String packageName(File file) {
@@ -252,6 +253,50 @@ public class StringSearch {
         } catch (FileNotFoundException e) {
             return new BufferedReader(new StringReader(""));
         }
+    }
+
+    public static double similarity(String s1, String s2) {
+        if (s2.length() < s1.length()) {
+            s1 = s1.substring(0, s2.length());
+        }
+        String longer = s1, shorter = s2;
+        if (s1.length() < s2.length()) { // longer should always have greater length
+            longer = s2;
+            shorter = s1;
+        }
+
+        int longerLength = longer.length();
+        if (longerLength == 0) {
+            return 1.0; /* both strings are zero length */
+        }
+        return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
+    }
+
+    public static int editDistance(String s1, String s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        int[] costs = new int[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            int lastValue = i;
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0) {
+                    costs[j] = j;
+                } else {
+                    if (j > 0) {
+                        int newValue = costs[j - 1];
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
+            }
+            if (i > 0) {
+                costs[s2.length()] = lastValue;
+            }
+        }
+        return costs[s2.length()];
     }
 
     private static boolean containsString(Path java, String query) {
