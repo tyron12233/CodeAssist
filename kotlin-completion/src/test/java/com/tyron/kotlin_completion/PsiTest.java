a60fb4cda6311c2d518f8f97eb8e770f22fff77b
tyron12233 <lucena.tyronscott@gmail.com>
2021-12-15 13:28:21 +0800

    test: comment out psi test

--- kotlin-completion/src/test/java/com/tyron/kotlin_completion/PsiTest.java
+++ kotlin-completion/src/test/java/com/tyron/kotlin_completion/PsiTest.java
@@ -1,32 +1,16 @@
 package com.tyron.kotlin_completion;
 
-import org.jetbrains.kotlin.cli.common.environment.UtilKt;
 import org.jetbrains.kotlin.com.intellij.core.CoreApplicationEnvironment;
 import org.jetbrains.kotlin.com.intellij.core.JavaCoreApplicationEnvironment;
 import org.jetbrains.kotlin.com.intellij.core.JavaCoreProjectEnvironment;
-import org.jetbrains.kotlin.com.intellij.lang.java.JavaLanguage;
 import org.jetbrains.kotlin.com.intellij.openapi.Disposable;
 import org.jetbrains.kotlin.com.intellij.openapi.project.Project;
 import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer;
 import org.jetbrains.kotlin.com.intellij.psi.JavaModuleSystem;
-import org.jetbrains.kotlin.com.intellij.psi.PsiClass;
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement;
 import org.jetbrains.kotlin.com.intellij.psi.PsiElementFinder;
-import org.jetbrains.kotlin.com.intellij.psi.PsiFile;
-import org.jetbrains.kotlin.com.intellij.psi.PsiFileFactory;
-import org.jetbrains.kotlin.com.intellij.psi.PsiMethod;
-import org.jetbrains.kotlin.com.intellij.psi.PsiReferenceExpression;
-import org.jetbrains.kotlin.com.intellij.psi.PsiType;
 import org.jetbrains.kotlin.com.intellij.psi.augment.PsiAugmentProvider;
-import org.jetbrains.kotlin.com.intellij.psi.impl.file.impl.JavaFileManager;
-import org.jetbrains.kotlin.com.intellij.psi.search.GlobalSearchScope;
-import org.jetbrains.kotlin.com.intellij.psi.util.PsiTypesUtil;
-import org.jetbrains.kotlin.com.intellij.psi.util.PsiUtil;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.File;
-import java.util.Arrays;
 
 public class PsiTest {
 
@@ -60,11 +44,11 @@ public class PsiTest {
 //                factory.createFileFromText(JavaLanguage.INSTANCE, text2);
 //        PsiElement element = file2.findElementAt(23);
 //        System.out.println(element.getContext().getReference());
-
-        UtilKt.setIdeaIoUseFallback();
-        mProjectEnvironment.addJarToClassPath(new File("/home/tyron/AndroidStudioProjects/CodeAssist/build-logic/src/test/resources/bootstraps/rt.jar"));
-        PsiClass aClass = JavaFileManager.getInstance(mProject)
-                .findClass("java.lang.String", GlobalSearchScope.allScope(mProject));
-        System.out.println(aClass);
+//
+//        UtilKt.setIdeaIoUseFallback();
+//        mProjectEnvironment.addJarToClassPath(new File("/home/tyron/AndroidStudioProjects/CodeAssist/build-logic/src/test/resources/bootstraps/rt.jar"));
+//        PsiClass aClass = JavaFileManager.getInstance(mProject)
+//                .findClass("java.lang.String", GlobalSearchScope.allScope(mProject));
+//        System.out.println(aClass);
     }
 }
