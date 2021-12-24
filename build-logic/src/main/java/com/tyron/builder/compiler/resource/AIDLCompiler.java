02e88c733aa90dcca8d550124fbc257c44ea91f8
deenu713 <82932744+deenu713@users.noreply.github.com>
2021-12-24 11:18:52 +0530

    Added Aidl Compliler

---
+++ build-logic/src/main/java/com/tyron/builder/compiler/resource/AIDLCompiler.java
@@ -0,0 +1,90 @@
+package com.test;
+//package com.tyron.builder.compiler.resource;
+
+import android.util.Log;
+
+import com.tyron.builder.BuildModule;
+import com.tyron.builder.exception.CompilationFailedException;
+import com.tyron.builder.log.ILogger;
+import com.tyron.builder.model.Project;
+import com.tyron.builder.parser.FileManager;
+import com.tyron.common.util.BinaryExecutor;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.FilenameFilter;
+
+public class AIDLCompiler {
+
+	private static final String TAG = AIDLCompiler.class.getSimpleName();
+
+	private final Project mProject;
+	private final ILogger mLogger;
+	File file = new File(mProject.getBuildDirectory(), "gen");
+
+	
+	public AIDLCompiler(ILogger log, Project project) {
+		mLogger = log;
+		mProject = project;
+	}
+
+	public void run() throws IOException, CompilationFailedException {
+		
+		compileProject();
+	
+		}
+	private void compileProject() throws IOException, CompilationFailedException {
+
+		mLogger.debug("Compiling project AIDL resources.");
+
+		FileManager.deleteDir(getOutputPath());
+		FileManager.deleteDir(new File(mProject.getBuildDirectory(), "gen"));
+
+		List<String> args = new ArrayList<>();
+		args.add(getBinary().getAbsolutePath());
+		args.add("");
+		args.add("-o");
+		args.add(file.getAbsolutePath());
+	
+		BinaryExecutor exec = new BinaryExecutor();
+		exec.setCommands(args);
+		if (!exec.execute().trim().isEmpty()) {
+			throw new CompilationFailedException(exec.getLog());
+		}
+
+		
+
+	}
+
+	
+			
+	/**
+	 * Retrieves the package names of libraries of has a resource file
+	 * @return list of package names in a form of string separated by ":" for use with AAPT2 directly
+	 */
+	
+	/**
+	 * Retrieves the package name of a manifest (.xml) file
+	 * @return null if an exception occurred or cannot be determined.
+	 */
+	
+	private static File getBinary() throws IOException {
+		File check = new File(
+			BuildModule.getContext().getApplicationInfo().nativeLibraryDir,
+			"libaidl.so"
+		);
+		if (check.exists()) {
+			return check;
+		}
+
+		throw new IOException("AIDL Binary not found");
+	}
+}
