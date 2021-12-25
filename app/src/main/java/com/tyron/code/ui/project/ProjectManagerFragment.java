8254de523182df47d5ffaa511c659fee700175d9
hosnifraj0 <hosnifraj10@gmail.com>
2021-12-25 17:55:40 +0100

    fix: missing import

--- app/src/main/java/com/tyron/code/ui/project/ProjectManagerFragment.java
+++ app/src/main/java/com/tyron/code/ui/project/ProjectManagerFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
