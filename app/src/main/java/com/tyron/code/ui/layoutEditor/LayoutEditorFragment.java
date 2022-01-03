ca91bc5f8cca3537891ea93a9dc68ffe12e7674e
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-03 19:47:59 +0800

    feat: add required width and height attribtues automatically

--- app/src/main/java/com/tyron/code/ui/layoutEditor/LayoutEditorFragment.java
+++ app/src/main/java/com/tyron/code/ui/layoutEditor/LayoutEditorFragment.java
@@ -364,12 +364,16 @@ public class LayoutEditorFragment extends Fragment implements ProjectManager.OnP
     private List<ViewPalette> populatePalettes() {
         List<ViewPalette> palettes = new ArrayList<>();
 
-        palettes.add(createPalette("android.widget.LinearLayout", R.drawable.ic_baseline_vertical_24));
-        palettes.add(createPalette("android.widget.FrameLayout", R.drawable.ic_baseline_frame_24));
-        palettes.add(createPalette("android.widget.ScrollView", R.drawable.ic_baseline_format_line_spacing_24));
-        palettes.add(createPalette("android.widget.HorizontalScrollView", R.drawable.ic_baseline_format_line_spacing_24));
-
-        palettes.add(createPalette("androidx.cardview.widget.CardView", R.drawable.ic_baseline_style_24));
+        palettes.add(createPalette("android.widget.LinearLayout", R.drawable.ic_baseline_vertical_24,
+                ImmutableMap.of(Attributes.View.MinWidth, Dimension.valueOf("50dp"), Attributes.View.MinWidth, Dimension.valueOf("25dp"))));
+        palettes.add(createPalette("android.widget.FrameLayout", R.drawable.ic_baseline_frame_24,
+                ImmutableMap.of(Attributes.View.MinWidth, Dimension.valueOf("50dp"), Attributes.View.MinWidth, Dimension.valueOf("25dp"))));
+        palettes.add(createPalette("android.widget.ScrollView", R.drawable.ic_baseline_format_line_spacing_24,
+                ImmutableMap.of(Attributes.View.MinWidth, Dimension.valueOf("50dp"), Attributes.View.MinWidth, Dimension.valueOf("25dp"))));
+        palettes.add(createPalette("android.widget.HorizontalScrollView", R.drawable.ic_baseline_format_line_spacing_24,
+                ImmutableMap.of(Attributes.View.MinWidth, Dimension.valueOf("50dp"), Attributes.View.MinWidth, Dimension.valueOf("25dp"))));
+        palettes.add(createPalette("androidx.cardview.widget.CardView", R.drawable.ic_baseline_style_24,
+                ImmutableMap.of(Attributes.View.MinWidth, Dimension.valueOf("50dp"), Attributes.View.MinWidth, Dimension.valueOf("25dp"))));
 
         palettes.add(createPalette("android.widget.Button",
                 R.drawable.ic_baseline_crop_16_9_24,
@@ -402,10 +406,9 @@ public class LayoutEditorFragment extends Fragment implements ProjectManager.OnP
         ViewPalette.Builder builder = ViewPalette.builder()
                 .setClassName(className)
                 .setName(name)
-                .setIcon(icon)
-                .addDefaultValue(Attributes.View.MinHeight, Dimension.valueOf("25dp"))
-                .addDefaultValue(Attributes.View.MinWidth, Dimension.valueOf("50dp"));
-
+                .setIcon(icon);
+        builder.addDefaultValue(Attributes.View.Width, Dimension.valueOf("wrap_content"));
+        builder.addDefaultValue(Attributes.View.Height, Dimension.valueOf("wrap_content"));
         attributes.forEach(builder::addDefaultValue);
         return builder.build();
     }
