986f52bf71f2b6b0fd60cde06bfe36aecfbba83d
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-16 17:01:05 +0800

    feat: add suggestions of layout params based on the parent tag

--- xml-completion/src/main/java/com/tyron/completion/xml/StyleUtils.java
+++ xml-completion/src/main/java/com/tyron/completion/xml/StyleUtils.java
@@ -2,16 +2,21 @@ package com.tyron.completion.xml;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.google.common.collect.ImmutableSet;
 import com.tyron.completion.xml.model.DeclareStyleable;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -19,21 +24,60 @@ import java.util.TreeSet;
 public class StyleUtils {
 
     private static final Map<String, ImmutableSet<String>> sViewStyleMap = new HashMap<>();
+    private static final Map<String, ImmutableSet<String>> sLayoutParamsMap = new HashMap<>();
 
     static {
-        putStyle(View.class, "View", "ViewGroup_Layout", "ViewGroup_MarginLayout");
-        putStyle(ViewGroup.class, "ViewGroup", "View");
-        putStyle(LinearLayout.class, "LinearLayout", "ViewGroup");
-        putStyle(FrameLayout.class, "FrameLayout", "ViewGroup");
-        putStyle(RelativeLayout.class, "RelativeLayout", "RelativeLayout_Layout", "ViewGroup");
-        putStyle(TextView.class, "TextView", "View");
-        putStyle(Button.class, "Button", "CompoundButton", "TextView");
+        putLayoutParams(ViewGroup.class);
+        putLayoutParams(AbsoluteLayout.class);
+        putLayoutParams(FrameLayout.class);
+        putLayoutParams(RelativeLayout.class);
+        putLayoutParams(LinearLayout.class);
+
+        putStyle(View.class);
+        putStyle(ViewGroup.class);;
+        putStyle(LinearLayout.class);
+        putStyle(FrameLayout.class);
+        putStyle(RelativeLayout.class);
+        putStyle(TextView.class);
+        putStyle(Button.class);
+        putStyle(CompoundButton.class);
+    }
+
+    private static void putLayoutParams(@NonNull Class<? extends ViewGroup> viewGroup) {
+        ImmutableSet.Builder<String> builder = ImmutableSet.builder();
+        Class<?> current = viewGroup;
+        while (current != null) {
+            if ("java.lang.Object".equals(current.getName())) {
+                break;
+            }
+            // no layout params for view
+            if ("android.view.View".equals(current.getName())) {
+                break;
+            }
+
+            builder.add(current.getSimpleName() + "_Layout");
+            current = current.getSuperclass();
+        }
+        sLayoutParamsMap.put(viewGroup.getSimpleName() + "_Layout", builder.build());
     }
 
     private static void putStyle(Class<? extends View> view, String... styles) {
         ImmutableSet.Builder<String> builder = ImmutableSet.builder();
         for (String style : styles) {
             builder.add(style);
+        }
+        sViewStyleMap.put(view.getSimpleName(), builder.build());
+    }
+
+    private static void putStyle(@NonNull Class<? extends View> view) {
+        Class<?> current = view;
+        ImmutableSet.Builder<String> builder = ImmutableSet.builder();
+        while (current != null) {
+            if ("java.lang.Object".equals(current.getName())) {
+                break;
+            }
+            builder.add(current.getSimpleName());
+            current = current.getSuperclass();
         }
         sViewStyleMap.put(view.getSimpleName(), builder.build());
     }
@@ -57,6 +101,30 @@ public class StyleUtils {
             }
         }
         return styles;
+    }
+
+    public static Set<DeclareStyleable> getLayoutParam(Map<String, DeclareStyleable> map, String name) {
+        Set<DeclareStyleable> params = new HashSet<>();
+        String simpleName = getSimpleName(name);
+        if (!simpleName.endsWith("_Layout")) {
+            simpleName += "_Layout";;
+        }
+        if (!map.containsKey(simpleName)) {
+            return params;
+        }
+        params.add(map.get(simpleName));
+
+        ImmutableSet<String> strings = sLayoutParamsMap.get(simpleName);
+        if (strings != null) {
+            for (String string : strings) {
+                if (simpleName.equals(string)) {
+                    continue;
+                }
+
+                params.addAll(getLayoutParam(map, name));
+            }
+        }
+        return params;
     }
 
     private static String getSimpleName(String name) {
