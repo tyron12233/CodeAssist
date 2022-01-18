c057c2443e674706f5cb4e3a9f78fc3e0d5ddeaa
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-18 17:01:40 +0800

    feat: use better name for ImageEditor

--- app/src/main/java/com/tyron/code/ui/editor/impl/image/ImageEditor.java
+++ app/src/main/java/com/tyron/code/ui/editor/impl/image/ImageEditor.java
@@ -39,7 +39,7 @@ public class ImageEditor implements FileEditor {
     @NonNull
     @Override
     public String getName() {
-        return "image-editor";
+        return "Image Editor";
     }
 
     @Override
