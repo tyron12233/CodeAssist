d3bfb41a77e36784e6c6ee7a10e12159749afa80
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-20 16:17:40 +0800

    feat: add local variable action using the new action system

--- java-completion/src/main/java/com/tyron/completion/java/action/context/IntroduceLocalVariableAction.java
+++ java-completion/src/main/java/com/tyron/completion/java/action/context/IntroduceLocalVariableAction.java
@@ -1,16 +1,28 @@
 package com.tyron.completion.java.action.context;
 
+import android.content.Context;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 
+import com.android.tools.r8.internal.ev;
+import com.tyron.actions.AnAction;
+import com.tyron.actions.AnActionEvent;
+import com.tyron.actions.CommonDataKeys;
+import com.tyron.actions.Presentation;
 import com.tyron.completion.java.CompileTask;
+import com.tyron.completion.java.CompilerContainer;
+import com.tyron.completion.java.JavaCompilerService;
 import com.tyron.completion.java.R;
+import com.tyron.completion.java.action.CommonJavaContextKeys;
 import com.tyron.completion.java.action.api.Action;
 import com.tyron.completion.java.action.api.ActionContext;
 import com.tyron.completion.java.action.api.ActionProvider;
+import com.tyron.completion.java.action.util.RewriteUtil;
 import com.tyron.completion.java.rewrite.IntroduceLocalVariable;
+import com.tyron.completion.java.rewrite.Rewrite;
 import com.tyron.completion.java.util.ActionUtil;
+import com.tyron.editor.Editor;
 
 import org.openjdk.javax.lang.model.element.Element;
 import org.openjdk.javax.lang.model.element.ExecutableElement;
@@ -19,23 +31,75 @@ import org.openjdk.javax.lang.model.element.TypeParameterElement;
 import org.openjdk.javax.lang.model.type.TypeKind;
 import org.openjdk.javax.lang.model.type.TypeMirror;
 import org.openjdk.source.tree.Scope;
+import org.openjdk.source.tree.Tree;
 import org.openjdk.source.util.SourcePositions;
 import org.openjdk.source.util.TreePath;
 import org.openjdk.source.util.Trees;
 
+import java.io.File;
 import java.util.List;
 
-public class IntroduceLocalVariableAction extends ActionProvider {
+public class IntroduceLocalVariableAction extends AnAction {
+
+    public static final String ID = "javaIntroduceLocalVariableAction";
 
     @Override
-    public boolean isApplicable(ActionContext context, @NonNull TreePath currentPath) {
-        return ActionUtil.canIntroduceLocalVariable(currentPath);
+    public void update(@NonNull AnActionEvent event) {
+        Presentation presentation = event.getPresentation();
+        presentation.setVisible(false);
+
+        Editor editor = event.getData(CommonDataKeys.EDITOR);
+        if (editor == null) {
+            return;
+        }
+
+        JavaCompilerService compiler = event.getData(CommonJavaContextKeys.COMPILER);
+        if (compiler == null) {
+            return;
+        }
+        if (!compiler.isReady()) {
+            return;
+        }
+
+        File file = event.getData(CommonDataKeys.FILE);
+        if (file == null) {
+            return;
+        }
+
+        TreePath currentPath = event.getData(CommonJavaContextKeys.CURRENT_PATH);
+        if (currentPath == null) {
+            return;
+        }
+
+        if (!ActionUtil.canIntroduceLocalVariable(currentPath)) {
+            return;
+        }
+
+        presentation.setVisible(true);
+        presentation.setText(event.getDataContext().getString(R.string.menu_quickfix_introduce_local_variable_title));
     }
 
     @Override
-    public void addMenus(@NonNull ActionContext context) {
-        CompileTask task = context.getCompileTask();
-        TreePath path = context.getCurrentPath();
+    public void actionPerformed(@NonNull AnActionEvent e) {
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+        TreePath currentPath = e.getData(CommonJavaContextKeys.CURRENT_PATH);
+        File file = e.getData(CommonDataKeys.FILE);
+        JavaCompilerService compiler = e.getData(CommonJavaContextKeys.COMPILER);
+        CompilerContainer cachedContainer = compiler.getCachedContainer();
+
+        Rewrite rewrite = cachedContainer.get(task -> {
+            if (task != null) {
+                return performInternal(task, currentPath, file);
+            }
+            return null;
+        });
+
+        if (rewrite != null) {
+            RewriteUtil.performRewrite(editor, file, compiler, rewrite);
+        }
+    }
+
+    private Rewrite performInternal(CompileTask task, TreePath path, File file) {
         Element element = Trees.instance(task.task).getElement(path);
         if (element instanceof ExecutableElement) {
             TypeMirror returnType = ActionUtil.getReturnType(task.task, path,
@@ -44,13 +108,10 @@ public class IntroduceLocalVariableAction extends ActionProvider {
                 SourcePositions pos = Trees.instance(task.task).getSourcePositions();
                 long startPosition = pos.getStartPosition(path.getCompilationUnit(),
                         path.getLeaf());
-                String title = context.getContext().getString(R.string.menu_quickfix_introduce_local_variable_title);
-                MenuItem item = context.addMenu("context", title);
-                item.setOnMenuItemClickListener(i -> {
-                    context.performAction(new Action(new IntroduceLocalVariable(context.getCurrentFile(), element.getSimpleName().toString(), returnType, startPosition)));
-                    return true;
-                });
+                return new IntroduceLocalVariable(file.toPath(),
+                        element.getSimpleName().toString(), returnType, startPosition);
             }
         }
+        return null;
     }
 }
