25b57ae3a0a95144dfe20392d29d41bdc4fc949c
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-20 16:18:03 +0800

    feat: add support for importing inner classes

--- java-completion/src/main/java/com/tyron/completion/java/action/quickfix/ImportClassFieldFix.java
+++ java-completion/src/main/java/com/tyron/completion/java/action/quickfix/ImportClassFieldFix.java
@@ -6,12 +6,22 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.tyron.actions.AnAction;
+import com.tyron.actions.AnActionEvent;
+import com.tyron.actions.CommonDataKeys;
+import com.tyron.actions.Presentation;
+import com.tyron.completion.java.JavaCompilerService;
 import com.tyron.completion.java.R;
+import com.tyron.completion.java.action.CommonJavaContextKeys;
 import com.tyron.completion.java.action.api.Action;
 import com.tyron.completion.java.action.api.ActionContext;
 import com.tyron.completion.java.action.api.ActionProvider;
+import com.tyron.completion.java.action.util.RewriteUtil;
 import com.tyron.completion.java.rewrite.AddImport;
 import com.tyron.completion.java.rewrite.Rewrite;
+import com.tyron.completion.java.util.ActionUtil;
+import com.tyron.completion.java.util.DiagnosticUtil;
+import com.tyron.editor.Editor;
 
 import org.openjdk.javax.tools.Diagnostic;
 import org.openjdk.javax.tools.JavaFileObject;
@@ -19,61 +29,113 @@ import org.openjdk.tools.javac.api.ClientCodeWrapper;
 import org.openjdk.tools.javac.util.JCDiagnostic;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class ImportClassFieldFix extends ActionProvider {
+public class ImportClassFieldFix extends AnAction {
+
+    public static final String ID = "javaImportClassFieldFix";
 
     public static final String ERROR_CODE = "compiler.err.doesnt.exist";
 
     @Override
-    public boolean isApplicable(ActionContext context, @Nullable String errorCode) {
-        return ERROR_CODE.equals(errorCode);
+    public void update(@NonNull AnActionEvent event) {
+        Presentation presentation = event.getPresentation();
+        presentation.setVisible(false);
+
+        Diagnostic<?> diagnostic = event.getData(CommonJavaContextKeys.DIAGNOSTIC);
+        if (diagnostic == null) {
+            return;
+        }
+
+        ClientCodeWrapper.DiagnosticSourceUnwrapper diagnosticSourceUnwrapper =
+                DiagnosticUtil.getDiagnosticSourceUnwrapper(diagnostic);
+        if (diagnosticSourceUnwrapper == null) {
+            return;
+        }
+
+        if (!ERROR_CODE.equals(diagnostic.getCode())) {
+            return;
+        }
+
+        JavaCompilerService compiler = event.getData(CommonJavaContextKeys.COMPILER);
+        if (compiler == null) {
+            return;
+        }
+
+        String simpleName = String.valueOf(diagnosticSourceUnwrapper.d.getArgs()[1]);
+        List<String> classNames = new ArrayList<>();
+        for (String qualifiedName : compiler.publicTopLevelTypes()) {
+            if (qualifiedName.endsWith("." + simpleName)) {
+                classNames.add(qualifiedName);
+            }
+        }
+
+        if (classNames.size() > 1) {
+            presentation.setText(event.getDataContext().getString(R.string.import_class_title));
+        } else {
+            String format = event.getDataContext().getString(R.string.import_class_name,
+                    ActionUtil.getSimpleName(classNames.iterator().next()));
+            presentation.setText(format);
+        }
+
+        presentation.setEnabled(true);
+        presentation.setVisible(true);
     }
 
     @Override
-    public void addMenus(@NonNull ActionContext context) {
-        Diagnostic<? extends JavaFileObject> diagnostic = context.getDiagnostic();
-        if (!(diagnostic instanceof ClientCodeWrapper.DiagnosticSourceUnwrapper)) {
+    public void actionPerformed(@NonNull AnActionEvent e) {
+        Diagnostic<?> diagnostic = e.getData(CommonJavaContextKeys.DIAGNOSTIC);
+        diagnostic = DiagnosticUtil.getDiagnosticSourceUnwrapper(diagnostic);
+        if (diagnostic == null) {
             return;
         }
+
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
         JCDiagnostic d = ((ClientCodeWrapper.DiagnosticSourceUnwrapper) diagnostic).d;
+        String simpleName= String.valueOf(d.getArgs()[1]);
+        JavaCompilerService compiler = e.getData(CommonJavaContextKeys.COMPILER);
+        Path file = e.getData(CommonDataKeys.FILE).toPath();
 
-        Path file = context.getCurrentFile();
-        String title = context.getContext().getString(R.string.import_class_title);
-        MenuItem item = context.addMenu("quickFix", title);
-        item.setOnMenuItemClickListener(i -> {
-            String simpleName= String.valueOf(d.getArgs()[0]);
-            boolean isField = simpleName.contains(".");
-            String searchName = simpleName;
-            if (isField) {
-                searchName = searchName.substring(0, searchName.indexOf('.'));
-            }
+        boolean isField = simpleName.contains(".");
+        String searchName = simpleName;
+        if (isField) {
+            searchName = searchName.substring(0, searchName.indexOf('.'));
+        }
 
-            Map<String, Rewrite> map = new TreeMap<>();
-            for (String qualifiedName : context.getCompiler().publicTopLevelTypes()) {
+        Map<String, Rewrite> map = new TreeMap<>();
+        for (String qualifiedName : compiler.publicTopLevelTypes()) {
+            if (qualifiedName.endsWith("." + simpleName)) {
                 if (qualifiedName.endsWith("." + searchName)) {
                     if (isField) {
                         qualifiedName = qualifiedName.substring(0,
                                 qualifiedName.lastIndexOf('.'));
                         qualifiedName += simpleName;
                     }
-                    String name = context.getContext().getString(R.string.import_class_name, qualifiedName);
+                    String name = e.getDataContext()
+                            .getString(R.string.import_class_name, qualifiedName);
                     Rewrite addImport = new AddImport(file.toFile(), qualifiedName);
                     map.put(name, addImport);
                 }
             }
+        }
 
+        if (map.size() == 1) {
+            RewriteUtil.performRewrite(editor, file.toFile(), compiler,
+                    map.values().iterator().next());
+        } else {
             String[] titles = map.keySet().toArray(new String[0]);
-            new AlertDialog.Builder(context.getContext())
+            new AlertDialog.Builder(e.getDataContext())
                     .setTitle(R.string.import_class_title)
                     .setItems(titles, (di, w) -> {
                         Rewrite rewrite = map.get(titles[w]);
-                        context.performAction(new Action(rewrite));
+                        RewriteUtil.performRewrite(editor, file.toFile(),
+                                compiler, rewrite);
                     })
                     .setNegativeButton(android.R.string.cancel, null)
                     .show();
-            return true;
-        });
+        }
     }
 }
