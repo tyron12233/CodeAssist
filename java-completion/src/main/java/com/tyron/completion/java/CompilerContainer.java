9e092bf0e883462fc45da9ae70fd20b6d841283b
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-24 00:45:18 +0800

    feat: allow multiple threads to read CompileTask but only one thread at a time to write

--- java-completion/src/main/java/com/tyron/completion/java/CompilerContainer.java
+++ java-completion/src/main/java/com/tyron/completion/java/CompilerContainer.java
@@ -2,7 +2,14 @@ package com.tyron.completion.java;
 
 import androidx.annotation.GuardedBy;
 
+import org.jetbrains.kotlin.com.intellij.util.ReadMostlyRWLock;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -10,13 +17,39 @@ import kotlin.jvm.functions.Function1;
 
 /**
  * A container class for compiled information, used for thread safety
+ *
+ * A read is when the {@link CompileTask} is being accessed to get information about the parse tree.
+ * A write is when the {@link CompileTask} is being changed from a compile call
+ *
+ * Any threads are allowed to read only if there is no thread that is currently writing.
+ * If there is a thread that is currently writing, all other threads that attempts to read
+ * will be blocked until the write thread has finished.
+ *
+ * Only one thread is allowed to write at a time, during a write operation all threads that
+ * attempts to read will be blocked until the thread writing has finished.
  */
 public class CompilerContainer {
+
+    private volatile boolean mIsWriting;
 
     @GuardedBy("mLock")
     private volatile CompileTask mCompileTask;
 
     private final Object mLock = new Object();
+    private final List<Thread> mReaders = Collections.synchronizedList(new ArrayList<>());
+
+
+    public CompilerContainer() {
+
+    }
+
+    private void closeIfEmpty() {
+        if (mReaders.isEmpty()) {
+            if (mCompileTask != null) {
+                mCompileTask.close();
+            }
+        }
+    }
 
     /**
      * This is for codes that will use the compile information,
@@ -24,28 +57,58 @@ public class CompilerContainer {
      * are synchronized
      */
     public void run(Consumer<CompileTask> consumer) {
-        synchronized (mLock) {
-            try {
-                consumer.accept(mCompileTask);
-            } finally {
-                mCompileTask.close();
-            }
+        waitForWriter();
+        mReaders.add(Thread.currentThread());
+        try {
+            consumer.accept(mCompileTask);
+        } finally {
+            mReaders.remove(Thread.currentThread());
         }
     }
 
     public <T> T get(Function1<CompileTask, T> fun) {
-        synchronized (mLock) {
-            try {
-                return fun.invoke(mCompileTask);
-            } finally {
-                mCompileTask.close();
-            }
+        waitForWriter();
+        mReaders.add(Thread.currentThread());
+        try {
+            return fun.invoke(mCompileTask);
+        } finally {
+            mReaders.remove(Thread.currentThread());
         }
     }
 
     void initialize(Runnable runnable) {
         synchronized (mLock) {
-            runnable.run();
+            assertIsNotReader();
+            waitForReaders();
+            try {
+                mIsWriting = true;
+                runnable.run();
+            } finally {
+                mIsWriting = false;
+            }
+        }
+    }
+
+    private void waitForReaders() {
+        while (true) {
+            if (mReaders.isEmpty()) {
+                closeIfEmpty();
+                return;
+            }
+        }
+    }
+
+    private void waitForWriter() {
+        while (true) {
+            if (!mIsWriting) {
+                return;
+            }
+        }
+    }
+
+    private void assertIsNotReader() {
+        if (mReaders.contains(Thread.currentThread())) {
+            throw new RuntimeException("Cannot compile inside a container.");
         }
     }
 
