82e83d2eb351710d3976d27a4232b8e8557153fd
tyron12233 <lucena.tyronscott@gmail.com>
2022-01-29 06:45:45 +0800

    feat: add json brace handlers and indent handler

--- app/src/main/java/com/tyron/code/ui/editor/language/json/JsonLanguage.java
+++ app/src/main/java/com/tyron/code/ui/editor/language/json/JsonLanguage.java
@@ -4,12 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonToken;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+import org.json.JSONTokener;
 
 import io.github.rosemoe.sora.interfaces.AutoCompleteProvider;
 import io.github.rosemoe.sora.interfaces.CodeAnalyzer;
 import io.github.rosemoe.sora.interfaces.EditorLanguage;
 import io.github.rosemoe.sora.interfaces.NewlineHandler;
 import io.github.rosemoe.sora.langs.EmptyLanguage;
+import io.github.rosemoe.sora.langs.java.JavaTextTokenizer;
+import io.github.rosemoe.sora.langs.java.Tokens;
+import io.github.rosemoe.sora.text.TextUtils;
 import io.github.rosemoe.sora.widget.CodeEditor;
 import io.github.rosemoe.sora.widget.SymbolPairMatch;
 
@@ -38,12 +46,21 @@ public class JsonLanguage implements EditorLanguage {
 
     @Override
     public int getIndentAdvance(String content) {
-        return 0;
+        JSONLexer lexer = new JSONLexer(CharStreams.fromString(content));
+        Token token;
+        int advance = 0;
+        while ((token = lexer.nextToken()).getType() != Token.EOF) {
+            if (token.getType() == JSONLexer.LBRACKET) {
+                advance++;
+            }
+        }
+        advance = Math.max(0, advance);
+        return advance * 2;
     }
 
     @Override
     public boolean useTab() {
-        return true;
+        return false;
     }
 
     @Override
@@ -60,11 +77,44 @@ public class JsonLanguage implements EditorLanguage {
 
     @Override
     public SymbolPairMatch getSymbolPairs() {
-        return null;
+        return new SymbolPairMatch.DefaultSymbolPairs();
     }
 
     @Override
     public NewlineHandler[] getNewlineHandlers() {
-        return new NewlineHandler[0];
+        return new NewlineHandler[] {
+                new IndentHandler("{", "}"),
+                new IndentHandler("[", "]")
+        };
+    }
+
+    class IndentHandler implements NewlineHandler {
+
+        private final String start;
+        private final String end;
+
+        public IndentHandler(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public boolean matchesRequirement(String beforeText, String afterText) {
+            return beforeText.endsWith(start) && afterText.startsWith(end);
+        }
+
+        @Override
+        public HandleResult handleNewline(String beforeText, String afterText, int tabSize) {
+            int count = TextUtils.countLeadingSpaceCount(beforeText, tabSize);
+            int advanceBefore = getIndentAdvance(beforeText);
+            int advanceAfter = getIndentAdvance(afterText);
+            String text;
+            StringBuilder sb = new StringBuilder("\n")
+                    .append(TextUtils.createIndent(count + advanceBefore, tabSize, useTab()))
+                    .append('\n')
+                    .append(text = TextUtils.createIndent(count + advanceAfter, tabSize, useTab()));
+            int shiftLeft = text.length() + 1;
+            return new HandleResult(sb, shiftLeft);
+        }
     }
 }
