0ba4deb6806cb011648a1e535bc60e2a55870090
tyron12233 <lucena.tyronscott@gmail.com>
2022-02-08 13:18:12 +0800

    feat: add action to crash manually to test saving before crash

--- app/src/main/java/com/tyron/code/ui/main/action/debug/CrashAction.java
+++ app/src/main/java/com/tyron/code/ui/main/action/debug/CrashAction.java
@@ -1,38 +1,29 @@
 package com.tyron.code.ui.main.action.debug;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
-import com.tyron.actions.ActionGroup;
 import com.tyron.actions.ActionPlaces;
 import com.tyron.actions.AnAction;
 import com.tyron.actions.AnActionEvent;
 import com.tyron.actions.Presentation;
-import com.tyron.code.BuildConfig;
 
-public class DebugActionGroup extends ActionGroup {
-
-    public static final String ID = "debugActionGroup";
+public class CrashAction extends AnAction {
 
     @Override
     public void update(@NonNull AnActionEvent event) {
         Presentation presentation = event.getPresentation();
         presentation.setVisible(false);
 
-        if (!BuildConfig.DEBUG) {
-            return;
-        }
-
         if (!ActionPlaces.MAIN_TOOLBAR.equals(event.getPlace())) {
             return;
         }
 
+        presentation.setText("Throw uncaught exception");
         presentation.setVisible(true);
-        presentation.setText("Debug");
     }
 
     @Override
-    public AnAction[] getChildren(@Nullable AnActionEvent e) {
-        return new AnAction[]{new LoadActionJarAction(), new LoadFileEditorProviderAction(), new CrashAction()};
+    public void actionPerformed(@NonNull AnActionEvent e) {
+        throw new RuntimeException("Application manually crashed.");
     }
 }
