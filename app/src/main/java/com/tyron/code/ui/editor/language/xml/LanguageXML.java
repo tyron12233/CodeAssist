06b851750aad136bec630055e7e6fe97d74bb3ee
tyron12233 <lucena.tyronscott@gmail.com>
2022-02-16 01:30:57 +0800

    feat: proper enter indent after tag autocomplete

--- app/src/main/java/com/tyron/code/ui/editor/language/xml/LanguageXML.java
+++ app/src/main/java/com/tyron/code/ui/editor/language/xml/LanguageXML.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.google.common.base.Strings;
 import com.tyron.builder.compiler.manifest.xml.XmlFormatPreferences;
 import com.tyron.builder.compiler.manifest.xml.XmlFormatStyle;
 import com.tyron.builder.compiler.manifest.xml.XmlPrettyPrinter;
@@ -41,150 +42,205 @@ import io.github.rosemoe.sora.widget.SymbolPairMatch;
 
 public class LanguageXML implements Language {
 
-	private final Editor mEditor;
+    private final Editor mEditor;
 
-	private final XMLAnalyzer mAnalyzer;
+    private final XMLAnalyzer mAnalyzer;
 
-	public LanguageXML(Editor codeEditor) {
-		mEditor = codeEditor;
+    public LanguageXML(Editor codeEditor) {
+        mEditor = codeEditor;
 
-		mAnalyzer = new XMLAnalyzer(codeEditor);
-	}
+        mAnalyzer = new XMLAnalyzer(codeEditor);
+    }
 
-	public boolean isAutoCompleteChar(char ch) {
-		return MyCharacter.isJavaIdentifierPart(ch)
-				|| ch == '<'
-				|| ch == '/'
-				|| ch == ':'
-				|| ch == '.';
-	}
+    public boolean isAutoCompleteChar(char ch) {
+        return MyCharacter.isJavaIdentifierPart(ch) ||
+               ch == '<' ||
+               ch == '/' ||
+               ch == ':' ||
+               ch == '.';
+    }
 
-	@Override
-	public boolean useTab() {
-		return true;
-	}
+    @Override
+    public boolean useTab() {
+        return true;
+    }
 
-	@Override
-	public CharSequence format(CharSequence text) {
-		XmlFormatPreferences preferences = XmlFormatPreferences.defaults();
-		File file = mEditor.getCurrentFile();
-		CharSequence formatted = null;
-		if ("AndroidManifest.xml".equals(file.getName())) {
-			formatted = XmlPrettyPrinter.prettyPrint(String.valueOf(text),
-					preferences, XmlFormatStyle.MANIFEST, "\n");
-		} else {
-			if (ProjectUtils.isLayoutXMLFile(file)) {
-				formatted = XmlPrettyPrinter.prettyPrint(String.valueOf(text),
-						preferences, XmlFormatStyle.LAYOUT, "\n");
-			} else if (ProjectUtils.isResourceXMLFile(file)) {
-				formatted = XmlPrettyPrinter.prettyPrint(String.valueOf(text),
-						preferences, XmlFormatStyle.RESOURCE, "\n");
-			}
-		}
-		if (formatted == null) {
-			formatted = text;
-		}
-		return formatted;
-	}
+    @Override
+    public CharSequence format(CharSequence text) {
+        XmlFormatPreferences preferences = XmlFormatPreferences.defaults();
+        File file = mEditor.getCurrentFile();
+        CharSequence formatted = null;
+        if ("AndroidManifest.xml".equals(file.getName())) {
+            formatted = XmlPrettyPrinter.prettyPrint(String.valueOf(text), preferences,
+                                                     XmlFormatStyle.MANIFEST, "\n");
+        } else {
+            if (ProjectUtils.isLayoutXMLFile(file)) {
+                formatted = XmlPrettyPrinter.prettyPrint(String.valueOf(text), preferences,
+                                                         XmlFormatStyle.LAYOUT, "\n");
+            } else if (ProjectUtils.isResourceXMLFile(file)) {
+                formatted = XmlPrettyPrinter.prettyPrint(String.valueOf(text), preferences,
+                                                         XmlFormatStyle.RESOURCE, "\n");
+            }
+        }
+        if (formatted == null) {
+            formatted = text;
+        }
+        return formatted;
+    }
 
-	@Override
-	public SymbolPairMatch getSymbolPairs() {
-		return new SymbolPairMatch.DefaultSymbolPairs();
-	}
+    @Override
+    public SymbolPairMatch getSymbolPairs() {
+        return new SymbolPairMatch.DefaultSymbolPairs();
+    }
 
-	@Override
-	public NewlineHandler[] getNewlineHandlers() {
-		return new NewlineHandler[]{new StartTagHandler()};
-	}
+    @Override
+    public NewlineHandler[] getNewlineHandlers() {
+        return new NewlineHandler[]{new StartTagHandler(), new EndTagHandler(),
+                new EndTagAttributeHandler()};
+    }
 
-	@Override
-	public void destroy() {
+    @Override
+    public void destroy() {
 
-	}
+    }
 
-	@NonNull
-	@Override
-	public AnalyzeManager getAnalyzeManager() {
-		return mAnalyzer;
-	}
+    @NonNull
+    @Override
+    public AnalyzeManager getAnalyzeManager() {
+        return mAnalyzer;
+    }
 
-	@Override
-	public int getInterruptionLevel() {
-		return INTERRUPTION_LEVEL_SLIGHT;
-	}
+    @Override
+    public int getInterruptionLevel() {
+        return INTERRUPTION_LEVEL_SLIGHT;
+    }
 
-	@Override
-	public void requireAutoComplete(@NonNull ContentReference content,
-									@NonNull CharPosition position,
-									@NonNull CompletionPublisher publisher,
-									@NonNull Bundle extraArguments) throws CompletionCancelledException {
-		String prefix = CompletionHelper.computePrefix(content, position, this::isAutoCompleteChar);
-		List<CompletionItem> items =
-				new XMLAutoCompleteProvider(mEditor).getAutoCompleteItems(prefix,
-						position.getLine(), position.getColumn());
-		if (items == null) {
-			return;
-		}
-		for (CompletionItem item : items) {
-			publisher.addItem(item);
-		}
-	}
+    @Override
+    public void requireAutoComplete(@NonNull ContentReference content,
+                                    @NonNull CharPosition position,
+                                    @NonNull CompletionPublisher publisher,
+                                    @NonNull Bundle extraArguments) throws CompletionCancelledException {
+        String prefix = CompletionHelper.computePrefix(content, position, this::isAutoCompleteChar);
+        List<CompletionItem> items =
+                new XMLAutoCompleteProvider(mEditor).getAutoCompleteItems(prefix,
+                                                                          position.getLine(),
+                                                                          position.getColumn());
+        if (items == null) {
+            return;
+        }
+        for (CompletionItem item : items) {
+            publisher.addItem(item);
+        }
+    }
 
-	@Override
-	public int getIndentAdvance(@NonNull ContentReference content, int line, int column) {
-		String text = content.getLine(line).substring(0, column);
-		return getIndentAdvance(text);
-	}
+    @Override
+    public int getIndentAdvance(@NonNull ContentReference content, int line, int column) {
+        String text = content.getLine(line)
+                .substring(0, column);
+        return getIndentAdvance(text);
+    }
 
-	public int getIndentAdvance(String content){
-		return getIndentAdvance(content, XMLLexer.DEFAULT_MODE, true);
-	}
+    public int getIndentAdvance(String content) {
+        return getIndentAdvance(content, XMLLexer.DEFAULT_MODE, true);
+    }
 
-	public int getIndentAdvance(String content, int mode, boolean ignore) {
-		XMLLexer lexer = new XMLLexer(CharStreams.fromString(content));
-		lexer.pushMode(mode);
+    public int getIndentAdvance(String content, int mode, boolean ignore) {
+        XMLLexer lexer = new XMLLexer(CharStreams.fromString(content));
+        lexer.pushMode(mode);
 
-		int advance = 0;
-		while (lexer.nextToken().getType() != Lexer.EOF) {
-			switch (lexer.getToken().getType()) {
-				case XMLLexer.OPEN:
-					advance++;
-					break;
-				case XMLLexer.SLASH_CLOSE:
-					if (!ignore) {
-						advance--;
-					}
-					break;
-			}
-		}
+        int advance = 0;
+        while (lexer.nextToken()
+                       .getType() != Lexer.EOF) {
+            switch (lexer.getToken()
+                    .getType()) {
+                case XMLLexer.OPEN:
+                    advance++;
+                    break;
+                case XMLLexer.SLASH_CLOSE:
+                    if (!ignore) {
+                        advance--;
+                    }
+                    break;
+            }
+        }
 
-		if (advance == 0 && mode != XMLLexer.INSIDE) {
-			return getIndentAdvance(content, XMLLexer.INSIDE, ignore);
-		}
+        if (advance == 0 && mode != XMLLexer.INSIDE) {
+            return getIndentAdvance(content, XMLLexer.INSIDE, ignore);
+        }
 
-		return advance * mEditor.getTabCount();
-	}
+        return advance * mEditor.getTabCount();
+    }
 
-	public int getFormatIndent(String line) {
-		return getIndentAdvance(line, XMLLexer.DEFAULT_MODE, false);
-	}
+    public int getFormatIndent(String line) {
+        return getIndentAdvance(line, XMLLexer.DEFAULT_MODE, false);
+    }
 
-	private class StartTagHandler implements NewlineHandler {
+    private class EndTagHandler implements NewlineHandler {
 
-		@Override
-		public boolean matchesRequirement(String beforeText, String afterText) {
-			Log.d("StartTagHandler", "beforeText: " + beforeText + " afterText: " + afterText);
-			return beforeText.trim().startsWith("<");
-		}
+        @Override
+        public boolean matchesRequirement(String beforeText, String afterText) {
+            String trim = beforeText.trim();
+            if (!trim.startsWith("<")) {
+                return false;
+            }
+            if (!trim.endsWith("/>")) {
+                return false;
+            }
+            return afterText.trim()
+                    .startsWith("<");
+        }
 
-		@Override
-		public NewlineHandleResult handleNewline(String beforeText, String afterText, int tabSize) {
-			int count = TextUtils.countLeadingSpaceCount(beforeText, tabSize);
-			String text;
-			StringBuilder sb = new StringBuilder()
-					.append("\n")
-					.append(text = TextUtils.createIndent(count + 4, tabSize, useTab()));
-			return new NewlineHandleResult(sb, 0);
-		}
-	}
+        @Override
+        public NewlineHandleResult handleNewline(String beforeText, String afterText, int tabSize) {
+            int count = TextUtils.countLeadingSpaceCount(beforeText, tabSize);
+            String middle;
+            StringBuilder sb = new StringBuilder();
+            sb.append('\n');
+            sb.append(TextUtils.createIndent(count + tabSize, tabSize, useTab()));
+            sb.append('\n');
+            sb.append(middle = TextUtils.createIndent(count, tabSize, useTab()));
+            return new NewlineHandleResult(sb, middle.length() + 1);
+        }
+    }
+
+    private class EndTagAttributeHandler implements NewlineHandler {
+
+        @Override
+        public boolean matchesRequirement(String beforeText, String afterText) {
+            return beforeText.trim()
+                           .endsWith(">") &&
+                   afterText.trim()
+                           .startsWith("</");
+        }
+
+        @Override
+        public NewlineHandleResult handleNewline(String beforeText, String afterText, int tabSize) {
+            int count = TextUtils.countLeadingSpaceCount(beforeText, tabSize);
+            String middle;
+            StringBuilder sb = new StringBuilder();
+            sb.append('\n');
+            sb.append(TextUtils.createIndent(count, tabSize, useTab()));
+            sb.append('\n');
+            sb.append(middle = TextUtils.createIndent(count - tabSize, tabSize, useTab()));
+            return new NewlineHandleResult(sb, middle.length() + 1);
+        }
+    }
+
+    private class StartTagHandler implements NewlineHandler {
+
+        @Override
+        public boolean matchesRequirement(String beforeText, String afterText) {
+            String trim = beforeText.trim();
+            return trim.startsWith("<") && !trim.endsWith(">");
+        }
+
+        @Override
+        public NewlineHandleResult handleNewline(String beforeText, String afterText, int tabSize) {
+            int count = TextUtils.countLeadingSpaceCount(beforeText, tabSize);
+            String text;
+            StringBuilder sb = new StringBuilder().append("\n")
+                    .append(TextUtils.createIndent(count + tabSize, tabSize, useTab()));
+            return new NewlineHandleResult(sb, 0);
+        }
+    }
 }
