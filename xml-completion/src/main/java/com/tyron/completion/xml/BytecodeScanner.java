cd05e70f94781f2e013a7da0a22eeeac1447d51e
tyron12233 <lucena.tyronscott@gmail.com>
2022-02-20 19:11:39 +0800

    chore: extract common properties to root project #238

--- xml-completion/src/main/java/com/tyron/completion/xml/BytecodeScanner.java
+++ xml-completion/src/main/java/com/tyron/completion/xml/BytecodeScanner.java
@@ -1,6 +1,7 @@
 package com.tyron.completion.xml;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
