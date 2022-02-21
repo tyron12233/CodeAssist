62023a9ff03a7e30d1eb2852ee51c6481013b64e
tyron12233 <lucena.tyronscott@gmail.com>
2022-02-22 04:54:28 +0800

    feat: improve shifting of diagnostic indices as the user types

--- app/src/main/java/com/tyron/code/ui/editor/language/DiagnosticSpanMapUpdater.java
+++ app/src/main/java/com/tyron/code/ui/editor/language/DiagnosticSpanMapUpdater.java
@@ -4,73 +4,95 @@ import com.tyron.builder.model.DiagnosticWrapper;
 
 import java.util.List;
 
+import io.github.rosemoe.sora.text.CharPosition;
+import io.github.rosemoe.sora.text.Content;
+import io.github.rosemoe.sora.text.ContentReference;
+import io.github.rosemoe.sora.text.Indexer;
+
 public class DiagnosticSpanMapUpdater {
 
-    public static void shiftDiagnosticsOnSingleLineInsert(List<DiagnosticWrapper> diagnostics, int line, int startCol, int endCol) {
-        int length = endCol - startCol;
+    public static void shiftDiagnosticsOnSingleLineInsert(List<DiagnosticWrapper> diagnostics,
+                                                          ContentReference ref,
+                                                          CharPosition start,
+                                                          CharPosition end) {
+        Content reference = ref.getReference();
+        Indexer indexer = reference.getIndexer();
+        int length = end.index - start.index;
         for (DiagnosticWrapper diagnostic : diagnostics) {
-            if (diagnostic.getStartLine() < line) {
+            if (!isValid(diagnostic)) {
                 continue;
             }
-            if (diagnostic.getStartLine() == line) {
-                if (diagnostic.getStartColumn() < startCol) {
-                    continue;
-                }
+            CharPosition startPosition =
+                    indexer.getCharPosition((int) diagnostic.getStartPosition());
+            CharPosition endPosition = indexer.getCharPosition((int) diagnostic.getEndPosition());
+
+            // diagnostic is located before the insertion index, its not included
+            if (endPosition.index <= end.index) {
+                continue;
             }
             diagnostic.setStartPosition(diagnostic.getStartPosition() + length);
             diagnostic.setEndPosition(diagnostic.getEndPosition() + length);
         }
     }
 
-    public static void shiftDiagnosticsOnSingleLineDelete(List<DiagnosticWrapper> diagnostics, int line, int startCol, int endCol) {
-        int length = endCol - startCol;
+    public static void shiftDiagnosticsOnSingleLineDelete(List<DiagnosticWrapper> diagnostics,
+                                                          ContentReference ref,
+                                                          CharPosition start,
+                                                          CharPosition end) {
+        Content reference = ref.getReference();
+        Indexer indexer = reference.getIndexer();
+        int length = end.index - start.index;
         for (DiagnosticWrapper diagnostic : diagnostics) {
-            if (diagnostic.getStartLine() < line) {
+            if (!isValid(diagnostic)) {
                 continue;
             }
-            if (diagnostic.getStartLine() == line)  {
-                if (diagnostic.getStartColumn() < startCol) {
-                    continue;
-                }
+            CharPosition startPosition =
+                    indexer.getCharPosition((int) diagnostic.getStartPosition());
+            CharPosition endPosition = indexer.getCharPosition((int) diagnostic.getEndPosition());
+            if (startPosition.index > start.index) {
+                diagnostic.setStartPosition(diagnostic.getStartPosition() - length);
+            }
+            if (endPosition.index > end.index) {
+                diagnostic.setEndPosition(diagnostic.getEndPosition() - length);
+            }
+        }
+    }
+
+    public static void shiftDiagnosticsOnMultiLineDelete(List<DiagnosticWrapper> diagnostics,
+                                                         ContentReference ref,
+                                                         CharPosition start,
+                                                         CharPosition end) {
+        int length = end.index - start.index;
+        for (DiagnosticWrapper diagnostic : diagnostics) {
+            if (!isValid(diagnostic)) {
+                continue;
+            }
+            if (diagnostic.getStartPosition() < end.index) {
+                continue;
             }
             diagnostic.setStartPosition(diagnostic.getStartPosition() - length);
             diagnostic.setEndPosition(diagnostic.getEndPosition() - length);
         }
     }
 
-    public static void shiftDiagnosticsOnMultiLineDelete(List<DiagnosticWrapper> diagnostics, int startLine, int startColumn, int endLine, int endColumn) {
-        int length = endLine - startLine;
+    public static void shiftDiagnosticsOnMultiLineInsert(List<DiagnosticWrapper> diagnostics,
+                                                         ContentReference ref,
+                                                         CharPosition start,
+                                                         CharPosition end) {
+        int length = end.index - start.index;
         for (DiagnosticWrapper diagnostic : diagnostics) {
-            if (diagnostic.getStartLine() < startLine) {
+            if (!isValid(diagnostic)) {
                 continue;
             }
-            if (diagnostic.getEndLine() < endLine) {
+            if (diagnostic.getEndPosition() < end.index) {
                 continue;
             }
-            int dLength = diagnostic.getEndColumn() - diagnostic.getStartColumn();
-            diagnostic.setStartLine(diagnostic.getStartLine() - length);
-            diagnostic.setEndLine(diagnostic.getEndLine() - length);
-            diagnostic.setPosition(DiagnosticWrapper.USE_LINE_POS);
+            diagnostic.setStartPosition(diagnostic.getStartPosition() + length);
+            diagnostic.setEndPosition(diagnostic.getEndPosition() + length);
         }
     }
 
-    public static void shiftDiagnosticsOnMultiLineInsert(List<DiagnosticWrapper> diagnostics, int startLine, int startColumn, int endLine, int endColumn) {
-        int length = endLine - startLine;
-        for (DiagnosticWrapper diagnostic : diagnostics) {
-            if (diagnostic.getStartLine() < startLine) {
-                continue;
-            }
-            if (diagnostic.getStartLine() == startLine) {
-                if (diagnostic.getStartColumn() <= startColumn) {
-                    continue;
-                }
-                if (diagnostic.getEndColumn() <= endColumn) {
-                    continue;
-                }
-            }
-            diagnostic.setStartLine(diagnostic.getStartLine() + length);
-            diagnostic.setEndLine(diagnostic.getEndLine() + length);
-            diagnostic.setPosition(DiagnosticWrapper.USE_LINE_POS);
-        }
+    public static boolean isValid(DiagnosticWrapper d) {
+        return d.getStartPosition() >= 0 && d.getEndPosition() >= 0;
     }
 }
