b7b2abb45f15501746e449eae938a92865b6aeb2
tyron12233 <lucena.tyronscott@gmail.com>
2022-02-24 00:30:08 +0800

    fix: close compile task before writing

--- java-completion/src/main/java/com/tyron/completion/java/compiler/CompilerContainer.java
+++ java-completion/src/main/java/com/tyron/completion/java/compiler/CompilerContainer.java
@@ -77,6 +77,9 @@ public class CompilerContainer {
 
     void initialize(Supplier<CompileTask> supplier) {
         mWriteLock.lock();
+        if (mCompileTask != null) {
+            mCompileTask.close();
+        }
         try {
             mCompileTask = supplier.get();
         } finally {
