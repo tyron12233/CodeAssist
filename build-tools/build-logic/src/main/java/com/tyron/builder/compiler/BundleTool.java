586a14b631179c4f362fdbbea19e3d0f9b1cdb51
deenu713 <82932744+deenu713@users.noreply.github.com>
2022-03-01 08:31:39 +0530

    Add files via upload

---
+++ build-tools/build-logic/src/main/java/com/tyron/builder/compiler/BundleTool.java
@@ -0,0 +1,48 @@
+package com.tyron.builder.compiler;
+
+
+import com.tyron.builder.BuildModule;
+import java.io.File;
+import java.util.ArrayList;
+import com.android.tools.build.bundletool.BundleToolMain;
+
+public class BundleTool {
+
+    public static class Mode {
+        public static int TEST = 0;
+        //ToDo add more modes
+    }
+
+
+    private final ArrayList<String> commands ;
+    private final String mApkInputPath ;
+    private final String mApkOutputPath;
+
+    public BundleTool(String inputPath ,String outputPath,int mode){
+        commands = new ArrayList<>();
+        mApkInputPath = inputPath;
+        mApkOutputPath = outputPath;
+    }
+
+    //TODO: Adjust min and max sdk
+    public void aab() throws Exception {
+		commands.add("build-bundle");
+        commands.add("--modules=" + mApkInputPath);
+        commands.add("--output=" + mApkOutputPath);
+	
+        BundleTool.main(commands.toArray(new String[0]));
+    }
+	public void apk() throws Exception {
+		commands.add("build-apks");
+        commands.add("--bundle=" + mApkInputPath);
+        commands.add("--output=" + mApkOutputPath);
+        commands.add("--mode=universal");
+        commands.add("--aapt2=" + BuildModule.getContext().getApplicationInfo().nativeLibraryDir + "/libaapt2.so");
+
+		
+
+        BundleTool.main(commands.toArray(new String[0]));
+    }
+	
+
+    }
