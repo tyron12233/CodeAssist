80c2447bc2486375cb9ebb8fedaef3816e2fdc15
tyron12233 <lucena.tyronscott@gmail.com>
2022-03-17 20:12:12 +0800

    feat: use proper notification icon #274

--- app/src/main/java/com/tyron/code/service/CompilerService.java
+++ app/src/main/java/com/tyron/code/service/CompilerService.java
@@ -121,7 +121,11 @@ public class CompilerService extends Service {
     }
 
     private Notification setupNotification() {
-        return new NotificationCompat.Builder(this, createNotificationChannel()).setContentTitle(getString(R.string.app_name)).setSmallIcon(R.drawable.ic_launcher).setContentText("Preparing").setPriority(NotificationCompat.PRIORITY_HIGH).setOngoing(true).setProgress(100, 0, true).build();
+        return new NotificationCompat.Builder(this, createNotificationChannel())
+                .setContentTitle(getString(R.string.app_name)).setSmallIcon(R.drawable.ic_stat_code)
+                .setContentText("Preparing").setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setOngoing(true)
+                .setProgress(100, 0, true).build();
     }
 
     private void updateNotification(String title, String message, int progress) {
@@ -131,7 +135,10 @@ public class CompilerService extends Service {
     private void updateNotification(String title, String message, int progress, int priority) {
         new Handler(Looper.getMainLooper()).post(() -> {
             NotificationCompat.Builder builder =
-                    new NotificationCompat.Builder(this, "Compiler").setContentTitle(title).setContentText(message).setSmallIcon(R.drawable.ic_launcher).setPriority(priority);
+                    new NotificationCompat.Builder(this, "Compiler")
+                            .setContentTitle(title).setContentText(message)
+                            .setSmallIcon(R.drawable.ic_stat_code)
+                            .setPriority(priority);
             if (progress != -1) {
                 builder.setProgress(100, progress, false);
             }
@@ -141,7 +148,10 @@ public class CompilerService extends Service {
 
     private String createNotificationChannel() {
         NotificationChannelCompat channel = new NotificationChannelCompat.Builder("Compiler",
-                NotificationManagerCompat.IMPORTANCE_HIGH).setName("Compiler service").setDescription("Foreground notification for the compiler").build();
+                NotificationManagerCompat.IMPORTANCE_HIGH)
+                .setName("Compiler service")
+                .setDescription("Foreground notification for the compiler")
+                .build();
 
         NotificationManagerCompat.from(this).createNotificationChannel(channel);
 
@@ -249,7 +259,7 @@ public class CompilerService extends Service {
             if (shouldShowNotification) {
                 mMainHandler.post(() -> {
                     NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
-                            "Compiler").setSmallIcon(R.drawable.ic_launcher).setContentTitle(projectName).setContentText(getString(R.string.compilation_result_success));
+                            "Compiler").setSmallIcon(R.drawable.ic_stat_code).setContentTitle(projectName).setContentText(getString(R.string.compilation_result_success));
 
                     if (type != BuildType.AAB) {
                         Intent intent = new Intent(Intent.ACTION_VIEW);
