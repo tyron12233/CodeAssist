54ee0d8748ad83de966cef1874592488e0751b52
tyron12233 <lucena.tyronscott@gmail.com>
2022-03-19 18:58:27 +0800

    feat: output mapping file, reuse dex files from previous compilation on release mode

--- build-tools/build-logic/src/main/java/com/tyron/builder/compiler/dex/R8Task.java
+++ build-tools/build-logic/src/main/java/com/tyron/builder/compiler/dex/R8Task.java
@@ -1,16 +1,28 @@
 package com.tyron.builder.compiler.dex;
 
+import androidx.annotation.NonNull;
+
+import com.android.tools.r8.ArchiveClassFileProvider;
+import com.android.tools.r8.ClassFileResourceProvider;
 import com.android.tools.r8.CompilationMode;
+import com.android.tools.r8.D8;
+import com.android.tools.r8.D8Command;
 import com.android.tools.r8.OutputMode;
+import com.android.tools.r8.ProgramResource;
 import com.android.tools.r8.R8;
 import com.android.tools.r8.R8Command;
+import com.android.tools.r8.inspector.Inspector;
 import com.android.tools.r8.origin.Origin;
 import com.tyron.builder.compiler.BuildType;
 import com.tyron.builder.compiler.Task;
 import com.tyron.builder.exception.CompilationFailedException;
 import com.tyron.builder.log.ILogger;
+import com.tyron.builder.model.Library;
 import com.tyron.builder.project.Project;
 import com.tyron.builder.project.api.AndroidModule;
+import com.tyron.common.util.AndroidUtilities;
+
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +31,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class R8Task extends Task<AndroidModule> {
 
@@ -40,12 +56,13 @@ public class R8Task extends Task<AndroidModule> {
 
     @Override
     public void run() throws IOException, CompilationFailedException {
-        getLogger().debug("Running R8");
         try {
+            ensureDexedLibraries();
+
             File output = new File(getModule().getBuildDirectory(), "bin");
             R8Command.Builder command = R8Command.builder(new DexDiagnosticHandler(getLogger(), getModule()))
                     .addLibraryFiles(getLibraryFiles())
-                    .addProgramFiles(getJarFiles())
+                    .addProgramFiles(getDexFiles())
                     .addProgramFiles(D8Task.getClassFiles(new File(getModule().getBuildDirectory(),
                             "bin/kotlin/classes")))
                     .addProgramFiles(D8Task.getClassFiles(new File(getModule().getBuildDirectory(),
@@ -54,11 +71,104 @@ public class R8Task extends Task<AndroidModule> {
                     .addProguardConfigurationFiles(getProguardRules())
                     .setMinApiLevel(getModule().getMinSdk())
                     .setMode(CompilationMode.RELEASE)
+                    .setProguardMapOutputPath(output.toPath())
                     .setOutput(output.toPath(), OutputMode.DexIndexed);
             R8.run(command.build());
         } catch (com.android.tools.r8.CompilationFailedException e) {
             throw new CompilationFailedException(e);
         }
+    }
+
+    /**
+     * Ensures that all libraries of the project has been dex-ed
+     *
+     * @throws com.android.tools.r8.CompilationFailedException if the compilation has failed
+     */
+    protected void ensureDexedLibraries() throws com.android.tools.r8.CompilationFailedException {
+        List<File> libraries = getModule().getLibraries();
+
+        for (File lib : libraries) {
+            File parentFile = lib.getParentFile();
+            if (parentFile == null) {
+                continue;
+            }
+            File[] libFiles = lib.getParentFile().listFiles();
+            if (libFiles == null) {
+                if (!lib.delete()) {
+                    getLogger().warning("Failed to delete " + lib.getAbsolutePath());
+                }
+            } else {
+                File dex = new File(lib.getParentFile(), "classes.dex");
+                if (dex.exists()) {
+                    continue;
+                }
+                if (lib.exists()) {
+                    String message;
+                    Library library = getModule().getLibrary(parentFile.getName());
+                    if (library != null) {
+                        boolean declared = library.getDeclaration() != null;
+                        message = "Dexing library " +
+                                  (declared ? library.getDeclaration() : library.getSourceFile()
+                                          .getName());
+                    } else {
+                        message = "Dexing jar " + parentFile.getName();
+                    }
+                    getLogger().debug(message);
+                    D8Command command = D8Command.builder(new DexDiagnosticHandler(getLogger(), getModule()))
+                            .addLibraryFiles(getLibraryFiles())
+                            .addClasspathFiles(libraries.stream().map(File::toPath)
+                                                       .collect(Collectors.toList()))
+                            .addProgramFiles(lib.toPath())
+                            .setMode(CompilationMode.RELEASE)
+                            .setMinApiLevel(getModule().getMinSdk())
+                            .setOutput(lib.getParentFile().toPath(), OutputMode.DexIndexed)
+                            .build();
+                    D8.run(command);
+                }
+            }
+        }
+    }
+
+    /**
+     * Performs dexing on the given jar file.
+     * Minification, desugaring and tree shaking are disabled, they will be performed
+     * later during the dex merging process
+     *
+     * @param jarFile The jar file
+     * @param outputDirectory The output directory where the files will be stored
+     */
+    private void dexLibrary(@NonNull File jarFile, @NonNull File outputDirectory) throws com.android.tools.r8.CompilationFailedException, IOException {
+        if (!jarFile.exists()) {
+            throw new IllegalArgumentException("Given file does not exist.");
+        }
+        if (!jarFile.getName().endsWith(".jar")) {
+            throw new IllegalArgumentException("Given file is not a jar file: " + jarFile.getName());
+        }
+
+        if (!outputDirectory.exists()) {
+            FileUtils.forceMkdir(outputDirectory);
+        }
+
+        D8Command.Builder command = D8Command.builder()
+                .setMode(CompilationMode.RELEASE)
+                .setOutput(outputDirectory.toPath(), OutputMode.DexIndexed);
+        command.addLibraryFiles(getLibraryFiles());
+        command.addClasspathFiles(getModule().getLibraries().stream()
+                                          .map(File::toPath)
+                                          .collect(Collectors.toList()));
+        command.addProgramFiles(jarFile.toPath());
+        command.setMinApiLevel(getModule().getMinSdk());
+        D8.run(command.build());
+    }
+
+    private List<Path> getDexFiles() {
+        List<File> libraries = getModule().getLibraries();
+        return libraries.stream()
+                .map(File::getParentFile)
+                .filter(Objects::nonNull)
+                .flatMap(it -> Arrays.stream(it.listFiles(c -> c.getName().endsWith(".dex"))))
+                .map(File::toPath)
+                .collect(Collectors.toList());
     }
 
     private List<String> getDefaultProguardRule() {
