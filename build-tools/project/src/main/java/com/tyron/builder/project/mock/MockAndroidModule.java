b89f9fdff6a3f07242cb42d1a6a5dfa49621d736
Mike Anderson <45513948+MikeAndrson@users.noreply.github.com>
2022-03-19 19:18:20 +0300

    fix: Don't create ModuleSettings every time getSettings is called

--- build-tools/project/src/main/java/com/tyron/builder/project/mock/MockAndroidModule.java
+++ build-tools/project/src/main/java/com/tyron/builder/project/mock/MockAndroidModule.java
@@ -29,13 +29,15 @@ public class MockAndroidModule extends MockJavaModule implements AndroidModule {
 
     private File mAndroidResourcesDir;
 
+    private final ModuleSettings mockSettings = new MockModuleSettings();
+
     public MockAndroidModule(File rootDir, FileManager fileManager) {
         super(rootDir, fileManager);
     }
 
     @Override
     public ModuleSettings getSettings() {
-        return new MockModuleSettings();
+        return mockSettings;
     }
 
     @Override
