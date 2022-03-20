868e328882f080ce70dbd27b08375bd17e8c8fbf
tyron12233 <lucena.tyronscott@gmail.com>
2022-03-21 00:30:57 +0800

    feat: better exception message for circular dependencies

--- build-tools/builder-api/src/main/java/com/tyron/builder/api/internal/tasks/CircularDependencyException.java
+++ build-tools/builder-api/src/main/java/com/tyron/builder/api/internal/tasks/CircularDependencyException.java
@@ -4,6 +4,10 @@ import com.tyron.builder.api.Task;
 
 public class CircularDependencyException extends RuntimeException {
 
+    public CircularDependencyException() {
+
+    }
+
     public CircularDependencyException(Task first, Task second) {
         super("Circular dependency detected: " + first + " <-> " + second);
     }
