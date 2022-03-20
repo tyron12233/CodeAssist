06a09006d0fa6bc1d24f17a796b3417c1f56bf6f
tyron12233 <lucena.tyronscott@gmail.com>
2022-03-20 23:47:48 +0800

    test: circular dependency checks

--- build-tools/builder-api/src/test/java/com/tyron/builder/api/TestTaskExecution.java
+++ build-tools/builder-api/src/test/java/com/tyron/builder/api/TestTaskExecution.java
@@ -1,49 +1,85 @@
 package com.tyron.builder.api;
 
-import com.google.common.collect.Ordering;
 import com.tyron.builder.api.internal.tasks.CircularDependencyException;
 import com.tyron.builder.api.internal.tasks.DefaultTaskContainer;
-import com.tyron.builder.api.internal.tasks.WorkDependencyResolver;
-import com.tyron.builder.api.tasks.TaskProvider;
+import com.tyron.builder.api.internal.tasks.TaskExecutor;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * WIP
  */
 public class TestTaskExecution {
 
+    private DefaultTaskContainer container;
+
+    @Before
+    public void setup() {
+        container = new DefaultTaskContainer();
+    }
+
     @Test
     public void testTaskDependency() {
-        DefaultTaskContainer container = new DefaultTaskContainer();
+        List<Task> executedTasks = new ArrayList<>();
+
         container.register("task1", task -> {
-            task.doLast(it -> System.out.println("Task 1"));
+            task.doLast(executedTasks::add);
             task.dependsOn("task2");
         });
 
         container.register("task2", task -> {
-            task.doLast(it -> System.out.println("Task 2"));
+            task.doLast(executedTasks::add);
         });
 
         container.register("task3", task -> {
-            task.doLast(it -> System.out.println("Task 3"));
+            task.doLast(executedTasks::add);
             task.dependsOn("task1");
         });
 
-        execute(container.resolveTask("task3"));
+        TaskExecutor taskExecutor = new TaskExecutor(container);
+        taskExecutor.execute("task3");
+
+        // task 3 -> task 1 -> task 2
+        assertExecutionOrder(executedTasks, "task2", "task1", "task3");
     }
 
-    private void execute(Task task) {
-        WorkDependencyResolver.TASK_AS_TASK
-                .resolve(task, task.getTaskDependencies(), dependency -> {
-                    if (task.equals(dependency)) {
-                        throw new CircularDependencyException();
-                    }
-                    execute(dependency);
-                });
-        task.getActions().forEach(action -> action.execute(task));
+    @Test
+    public void testNoCircularDependency() {
+        container.register("task1", task -> {
+            task.dependsOn("task2");
+        });
+        container.register("task2", task -> {
+            task.dependsOn("task1");
+        });
+        try {
+            TaskExecutor taskExecutor = new TaskExecutor(container);
+            taskExecutor.execute("task1");
+        } catch (CircularDependencyException expected) {
+            return;
+        }
 
-        WorkDependencyResolver.TASK_AS_TASK
-                .resolve(task, task.getMustRunAfter(), this::execute);
+        throw new AssertionError("Circular dependency was not detected.");
     }
+
+    private void assertExecutionOrder(List<Task> tasks, String... executionOrder) {
+        if (tasks.size() != executionOrder.length) {
+            throw new AssertionError("Expected " + executionOrder.length + " but got " + tasks.size());
+        }
+
+        for (int i = 0; i < executionOrder.length; i++) {
+            Task task = tasks.get(i);
+            String name = executionOrder[i];
+
+            if (!task.getName().equals(name)) {
+                throw new AssertionError("Execution order not met. Tasks ran: " + tasks + "\n" +
+                                         "Expected order: " + Arrays.toString(executionOrder));
+            }
+        }
+    }
+
 }
