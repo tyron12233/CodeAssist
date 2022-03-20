16aee96b8aac9705755e117041f2b81674f94603
tyron12233 <lucena.tyronscott@gmail.com>
2022-03-20 23:43:55 +0800

    refactor: move task execution logic to its own class

--- build-tools/builder-api/src/main/java/com/tyron/builder/api/internal/tasks/TaskExecutor.java
+++ build-tools/builder-api/src/main/java/com/tyron/builder/api/internal/tasks/TaskExecutor.java
@@ -1,72 +1,41 @@
 package com.tyron.builder.api.internal.tasks;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.graph.ElementOrder;
-import com.google.common.graph.EndpointPair;
-import com.google.common.graph.Graph;
-import com.google.common.graph.GraphBuilder;
-import com.google.common.graph.Graphs;
-import com.google.common.graph.ImmutableGraph;
-import com.google.common.graph.MutableGraph;
-import com.tyron.builder.api.Action;
 import com.tyron.builder.api.Task;
 import com.tyron.builder.api.tasks.TaskContainer;
-import com.tyron.builder.api.tasks.TaskDependency;
+import com.tyron.builder.api.tasks.TaskResolver;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-
-@SuppressWarnings("UnstableApiUsage")
 public class TaskExecutor {
 
-    private MutableGraph<Task> mTaskGraph;
+    private final TaskResolver resolver;
 
-    public TaskExecutor() {
-        mTaskGraph = GraphBuilder.directed()
-                .allowsSelfLoops(false)
-                .nodeOrder(ElementOrder.insertion())
-                .build();
+    public TaskExecutor(TaskResolver resolver) {
+        this.resolver = resolver;
     }
 
-    public void run(Task root) {
-        mTaskGraph.addNode(root);
-        buildDependencyGraph(root);
-        execute(mTaskGraph, root);
-    }
-
-    public void run(TaskContainer taskContainer, Task root) {
-        Set<? extends Task> dependencies = root.getTaskDependencies().getDependencies(root);
-        for (Task dependency : dependencies) {
-            run(taskContainer, dependency);
+    // TODO: handle cases when name is not found.
+    /**
+     * Resolves the task using the given path and executes it.
+     * @param path The task name
+     */
+    public void execute(String path) {
+        Task task = resolver.resolveTask(path);
+        if (task == null) {
+            throw new RuntimeException("Failed to resolve " + path);
         }
-
-        root.getActions().forEach(action -> action.execute(root));
-
-        Set<? extends Task> mustRunAfterSet = root.getMustRunAfter().getDependencies(root);
-        for (Task mustRunAfter : mustRunAfterSet) {
-            run(taskContainer, mustRunAfter);
-        }
+        execute(task);
     }
 
-    private void execute(Graph<Task> graph, Task root) {
-        ImmutableList<Task> reverse =
-                ImmutableList.copyOf(Graphs.reachableNodes(graph, root))
-                .reverse();
-        reverse.forEach(task -> {
-            task.getActions().forEach(action -> action.execute(task));
-        });
-    }
+    public void execute(Task task) {
+        WorkDependencyResolver.TASK_AS_TASK
+                .resolve(task, task.getTaskDependencies(), dependency -> {
+                    if (task.equals(dependency)) {
+                        throw new CircularDependencyException();
+                    }
+                    execute(dependency);
+                });
+        task.getActions().forEach(action -> action.execute(task));
 
-    private void buildDependencyGraph(Task root) {
-        TaskDependency taskDependencies = root.getTaskDependencies();
-        Set<? extends Task> dependencies = taskDependencies.getDependencies(root);
-        for (Task dependency : dependencies) {
-            mTaskGraph.putEdge(root, dependency);
-            mTaskGraph.addNode(root);
-            buildDependencyGraph(dependency);
-        }
+        WorkDependencyResolver.TASK_AS_TASK
+                .resolve(task, task.getMustRunAfter(), this::execute);
     }
 }
