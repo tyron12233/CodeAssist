4132c1b7c44a217aed8d6d9630bf7089162c2424
tyron12233 <lucena.tyronscott@gmail.com>
2022-04-04 04:44:11 +0800

    feat: snapshot serialization implementations

---
+++ build-tools/builder-api/src/main/java/com/tyron/builder/api/internal/snapshot/impl/ValueSnapshotterSerializerRegistry.java
@@ -0,0 +1,7 @@
+package com.tyron.builder.api.internal.snapshot.impl;
+
+
+import com.tyron.builder.api.internal.serialize.SerializerRegistry;
+
+public interface ValueSnapshotterSerializerRegistry extends SerializerRegistry {
+}
