daff56a756d70fb311cc18f31ec37e90ac6bbcd3
tyron12233 <lucena.tyronscott@gmail.com>
2022-04-05 14:07:43 +0800

    refactor: separate tests into its own classes

---
+++ build-tools/builder-api/src/test/java/com/tyron/builder/api/TestTaskInputOutputs.java
@@ -0,0 +1,41 @@
+package com.tyron.builder.api;
+
+import com.tyron.builder.api.project.BuildProject;
+import com.tyron.builder.api.tasks.TaskContainer;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestTaskInputOutputs extends TestTaskExecutionCase {
+    // Lambdas are not supported for gradle caching since java uses invokedynamic on them,
+    // making it impossible for gradle to track the class and its tasks
+    @SuppressWarnings("Convert2Lambda")
+    @Test
+    public void testTaskOutputs() {
+        Action<BuildProject> evaluationAction = project -> {
+
+            File outputDir = new File(project.getBuildDir(), "output");
+            File input = new File(project.getBuildDir().getParent(), "Test.java");
+
+            TaskContainer tasks = project.getTasks();
+            tasks.register("Task", new Action<Task>() {
+                @Override
+                public void execute(Task task) {
+                    task.getOutputs().dir(outputDir);
+                    task.getInputs().file(input);
+
+                    task.doLast(new Action<Task>() {
+                        @Override
+                        public void execute(Task task) {
+                            System.out.println("RUNNING");
+                        }
+                    });
+                }
+            });
+        };
+
+        evaluateProject(project, evaluationAction);
+        executeProject(project, "Task");
+    }
+}
