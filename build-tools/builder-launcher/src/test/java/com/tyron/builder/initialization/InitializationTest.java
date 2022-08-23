fd2a19b98bb33215f1fd772bf1bebe392e51d776
Tyron <lucena.tyronscott@gmail.com>
2022-08-24 00:37:36 +0800

    fix: allow com.tyron.build.* package on scripts

--- build-tools/builder-launcher/src/test/java/com/tyron/builder/initialization/InitializationTest.java
+++ build-tools/builder-launcher/src/test/java/com/tyron/builder/initialization/InitializationTest.java
@@ -1,25 +1,11 @@
 package com.tyron.builder.initialization;
 
-import org.gradle.internal.installation.CurrentGradleInstallation;
-import org.gradle.internal.installation.GradleInstallation;
-import org.gradle.internal.jvm.Jvm;
 import org.junit.Test;
-
-import java.io.File;
 
 public class InitializationTest {
 
     @Test
     public void testInitialization() {
-        Jvm current = Jvm.current();
-        CurrentGradleInstallation.setInstance(
-                new CurrentGradleInstallation(
-                        new GradleInstallation(
-                                new File("C:\\Users\\TyronScott\\" +
-                                         ".gradle\\wrapper\\dists\\gradle-7.4-bin\\c0gwcg53nkjbqw7r0h0umtfvt\\gradle-7.4")
-                        )
-                )
-        );
-        Launcher.main(new String[0]);
+
     }
 }
