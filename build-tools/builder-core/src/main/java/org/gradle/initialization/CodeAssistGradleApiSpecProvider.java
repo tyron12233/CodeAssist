6784c402c48101169bb19bf2025ab1d752d200a4
Tyron <lucena.tyronscott@gmail.com>
2022-08-24 00:37:54 +0800

    fix: allow com.tyron.build.* package on scripts

--- build-tools/builder-core/src/main/java/org/gradle/initialization/CodeAssistGradleApiSpecProvider.java
+++ build-tools/builder-core/src/main/java/org/gradle/initialization/CodeAssistGradleApiSpecProvider.java
@@ -28,7 +28,7 @@ public class CodeAssistGradleApiSpecProvider extends GradleApiSpecProvider.SpecA
     @Override
     public Set<String> getExportedPackages() {
         return ImmutableSet.of(
-                "com.tyron.builder.plugin",
+                "com.tyron.builder",
                 "org.gradle",
                 "org.apache.tools.ant",
                 "groovy",
