24cf81e04b3fcc0c38a6e677715eb4a432f8a00f
Tyron <lucena.tyronscott@gmail.com>
2022-09-15 18:07:02 +0800

    fix: diagnostics not changing

--- app/src/main/java/com/tyron/code/ui/editor/impl/text/rosemoe/CodeAssistDiagnosticRegion.java
+++
@@ -1,4 +0,0 @@
-package com.tyron.code.ui.editor.impl.text.rosemoe;
-
-public class CodeAssistDiagnosticRegion {
-}
--- java-completion/src/main/java/com/tyron/completion/java/util/JavaDataContextUtil.java
+++ java-completion/src/main/java/com/tyron/completion/java/util/JavaDataContextUtil.java
@@ -3,28 +3,17 @@ package com.tyron.completion.java.util;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.sun.tools.javac.util.JCDiagnostic;
 import com.tyron.actions.CommonDataKeys;
 import com.tyron.actions.DataContext;
 import com.tyron.builder.model.DiagnosticWrapper;
-import com.tyron.builder.model.SourceFileObject;
 import com.tyron.builder.project.Project;
-import com.tyron.builder.project.api.JavaModule;
-import com.tyron.builder.project.api.Module;
-import com.tyron.common.ApplicationProvider;
 import com.tyron.common.SharedPreferenceKeys;
-import com.tyron.completion.index.CompilerService;
-import com.tyron.completion.java.JavaCompilerProvider;
-import com.tyron.completion.java.action.CommonJavaContextKeys;
-import com.tyron.completion.java.action.FindCurrentPath;
-import com.tyron.completion.java.compiler.CompilerContainer;
-import com.tyron.completion.java.compiler.JavaCompilerService;
 
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.util.TreePath;
+import com.tyron.completion.java.compiler.services.NBLog;
 import com.tyron.completion.java.parse.CompilationInfo;
 
 import java.io.File;
-import java.time.Instant;
 import java.util.List;
 
 import javax.tools.Diagnostic;
@@ -39,12 +28,12 @@ public class JavaDataContextUtil {
             if (compilationInfo != null) {
                 context.putData(CompilationInfo.COMPILATION_INFO_KEY, compilationInfo);
 
-                SourceFileObject fileObject = new SourceFileObject(file.toPath(), "", Instant.now());
-                List<Diagnostic<? extends JavaFileObject>> diagnostics =
-                        compilationInfo.impl.getDiagnostics(fileObject);
+                List<JCDiagnostic> diagnostics =
+                        NBLog.instance(compilationInfo.impl.getJavacTask().getContext())
+                                .getDiagnostics(file.toURI());
                 if (!diagnostics.isEmpty()) {
                     Diagnostic<? extends JavaFileObject> diagnostic =
-                            DiagnosticUtil.getDiagnostic(diagnostics, cursor);
+                            DiagnosticUtil.getJCDiagnostic(diagnostics, cursor);
                     if (diagnostic != null) {
                         context.putData(CommonDataKeys.DIAGNOSTIC, new DiagnosticWrapper(diagnostic));
                     }
